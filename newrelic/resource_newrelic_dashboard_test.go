e9eb2a17cc3afef0d9c36d08cc0c6c01284bc19d
Clay Smith <smithclay@gmail.com>
2018-01-23 09:32:48 -0800

    add dashboard resource

---
+++ newrelic/resource_newrelic_dashboard_test.go
@@ -0,0 +1,69 @@
+package newrelic
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	newrelic "source.datanerd.us/csmith/go-newrelic/api"
+)
+
+func TestAccNewRelicDashboard_Basic(t *testing.T) {
+	rName := acctest.RandString(5)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicAlertPolicyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckNewRelicDashboardConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicDashboardExists("newrelic_dashboard.foo"),
+					resource.TestCheckResourceAttr(
+						"newrelic_dashboard.foo", "title", fmt.Sprintf("tf-test-%s", rName)),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckNewRelicDashboardExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No dashboard ID is set")
+		}
+
+		client := testAccProvider.Meta().(*newrelic.Client)
+
+		id, err := strconv.ParseInt(rs.Primary.ID, 10, 32)
+		if err != nil {
+			return err
+		}
+
+		found, err := client.GetDashboard(int(id))
+		if err != nil {
+			return err
+		}
+
+		if strconv.Itoa(found.ID) != rs.Primary.ID {
+			return fmt.Errorf("Policy not found: %v - %v", rs.Primary.ID, found)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckNewRelicDashboardConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "newrelic_dashboard" "foo" {
+  title = "tf-test-%s"
+}
+`, rName)
+}
