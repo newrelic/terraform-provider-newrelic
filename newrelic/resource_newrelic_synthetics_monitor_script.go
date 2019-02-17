e73c812b872b0471cfda0e1bbd4ccfbe49c14ed1
Kevin Yeh <kevinyeah@utexas.edu>
2019-02-16 23:01:48 -0500

    Add newrelic_synthetics_monitor_script resource

---
+++ newrelic/resource_newrelic_synthetics_monitor_script.go
@@ -0,0 +1,111 @@
+package newrelic
+
+import (
+	"log"
+
+	synthetics "github.com/dollarshaveclub/new-relic-synthetics-go"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceNewRelicSyntheticsMonitorScript() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceNewRelicSyntheticsMonitorScriptCreate,
+		Read:   resourceNewRelicSyntheticsMonitorScriptRead,
+		Update: resourceNewRelicSyntheticsMonitorScriptUpdate,
+		Delete: resourceNewRelicSyntheticsMonitorScriptDelete,
+		Importer: &schema.ResourceImporter{
+			State: importSyntheticsMonitorScript,
+		},
+		Schema: map[string]*schema.Schema{
+			"monitor_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"text": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func importSyntheticsMonitorScript(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+	d.Set("monitor_id", d.Id())
+	return []*schema.ResourceData{d}, nil
+}
+
+func buildSyntheticsMonitorScriptStruct(d *schema.ResourceData) *synthetics.UpdateMonitorScriptArgs {
+	script := synthetics.UpdateMonitorScriptArgs{
+		ScriptText: d.Get("text").(string),
+	}
+
+	return &script
+}
+
+func resourceNewRelicSyntheticsMonitorScriptCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ProviderConfig).Synthetics
+	script := buildSyntheticsMonitorScriptStruct(d)
+
+	id := d.Get("monitor_id").(string)
+	log.Printf("[INFO] Creating New Relic Synthetics monitor script %s", id)
+
+	err := client.UpdateMonitorScript(id, script)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(id)
+	return resourceNewRelicSyntheticsMonitorScriptRead(d, meta)
+}
+
+func resourceNewRelicSyntheticsMonitorScriptRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ProviderConfig).Synthetics
+
+	log.Printf("[INFO] Reading New Relic Synthetics script %s", d.Id())
+
+	scriptText, err := client.GetMonitorScript(d.Id())
+	if err != nil {
+		if err == synthetics.ErrMonitorScriptNotFound {
+			d.SetId("")
+			return nil
+		}
+
+		return err
+	}
+
+	d.Set("text", scriptText)
+	return nil
+}
+
+func resourceNewRelicSyntheticsMonitorScriptUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ProviderConfig).Synthetics
+	script := buildSyntheticsMonitorScriptStruct(d)
+
+	log.Printf("[INFO] Creating New Relic Synthetics monitor script %s", d.Id())
+
+	err := client.UpdateMonitorScript(d.Id(), script)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(d.Id())
+	return resourceNewRelicSyntheticsMonitorScriptRead(d, meta)
+}
+
+func resourceNewRelicSyntheticsMonitorScriptDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ProviderConfig).Synthetics
+
+	log.Printf("[INFO] Deleting New Relic Synthetics monitor script %s", d.Id())
+
+	script := synthetics.UpdateMonitorScriptArgs{
+		ScriptText: " ",
+	}
+
+	if err := client.UpdateMonitorScript(d.Id(), &script); err != nil {
+		return err
+	}
+
+	d.SetId("")
+	return nil
+}
---
+++ newrelic/resource_newrelic_synthetics_monitor_script_test.go
@@ -0,0 +1,100 @@
+package newrelic
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccNewRelicSyntheticsMonitorScript_Basic(t *testing.T) {
+	rname := acctest.RandString(5)
+	scriptText := acctest.RandString(5)
+	scriptTextUpdated := acctest.RandString(5)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicSyntheticsMonitorScriptDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckNewRelicSyntheticsMonitorScriptConfig(rname, scriptText),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsMonitorScriptExists("newrelic_synthetics_monitor_script.foo_script"),
+					resource.TestCheckResourceAttr(
+						"newrelic_synthetics_monitor_script.foo_script", "text", scriptText),
+				),
+			},
+			{
+				Config: testAccCheckNewRelicSyntheticsMonitorScriptConfig(rname, scriptTextUpdated),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsMonitorScriptExists("newrelic_synthetics_monitor_script.foo_script"),
+					resource.TestCheckResourceAttr(
+						"newrelic_synthetics_monitor_script.foo_script", "text", scriptTextUpdated),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckNewRelicSyntheticsMonitorScriptExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No synthetics monitor script ID is set")
+		}
+
+		client := testAccProvider.Meta().(*ProviderConfig).Synthetics
+
+		foundText, err := client.GetMonitorScript(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if foundText != rs.Primary.Attributes["text"] {
+			return fmt.Errorf("Synthetics monitor script text does not match: %v \n\n %v", foundText, rs.Primary.Attributes["text"])
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckNewRelicSyntheticsMonitorScriptDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*ProviderConfig).Synthetics
+	for _, r := range s.RootModule().Resources {
+		if r.Type != "newrelic_synthetics_monitor_script" {
+			continue
+		}
+
+		foundText, err := client.GetMonitorScript(r.Primary.ID)
+		if err == nil || strings.TrimSpace(foundText) != "" {
+			return fmt.Errorf("Synthetics monitor script text still exists")
+		}
+	}
+	return nil
+}
+
+func testAccCheckNewRelicSyntheticsMonitorScriptConfig(rName string, scriptText string) string {
+	return fmt.Sprintf(`
+
+resource "newrelic_synthetics_monitor" "foo" {
+  name = "%[1]s"
+  type = "SCRIPT_BROWSER"
+  frequency = 1
+  status = "DISABLED"
+  locations = ["AWS_US_EAST_1"]
+  uri = "https://google.com"
+}
+
+resource "newrelic_synthetics_monitor_script" "foo_script" {
+  monitor_id = "${newrelic_synthetics_monitor.foo.id}"
+  text = "%[2]s"
+}
+`, rName, scriptText)
+}
