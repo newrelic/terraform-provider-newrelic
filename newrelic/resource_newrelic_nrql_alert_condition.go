dc7d721efe07192b9c00edbc34bf165d34393055
Nicole Yson <nicole.yson@vacasa.com>
2019-06-07 12:39:34 -0700

    discovered it was a string instead of an int

--- newrelic/resource_newrelic_nrql_alert_condition.go
+++ newrelic/resource_newrelic_nrql_alert_condition.go
@@ -50,9 +50,10 @@ func resourceNewRelicNrqlAlertCondition() *schema.Resource {
 							Required: true,
 						},
 						"since_value": {
-							Type:         schema.TypeString,
-							Required:     true,
-							ValidateFunc: validation.IntBetween(1, 20),
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateFunc: validation.StringInSlice([]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+								"11", "12", "13", "14", "15", "16", "17", "18", "19", "20"}, false),
 						},
 					},
 				},
