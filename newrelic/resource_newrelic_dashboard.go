5eb963c4b4131a3ea67b67a509cc0734574e529e
Chris Trombley <ctrombley@newrelic.com>
2019-11-11 13:18:01 -0800

    refactoring dashboard expansion and flattening

--- newrelic/resource_newrelic_dashboard.go
+++ newrelic/resource_newrelic_dashboard.go
@@ -1,12 +1,9 @@
 package newrelic
 
 import (
-	"bytes"
-	"fmt"
 	"log"
 	"strconv"
 
-	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 
@@ -73,9 +70,12 @@ func resourceNewRelicDashboard() *schema.Resource {
 				Type:     schema.TypeSet,
 				Optional: true,
 				MaxItems: 60,
-				Set:      resourceNewRelicDashboardWidgetsHash,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"widget_id": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
 						"title": {
 							Type:     schema.TypeString,
 							Required: true,
@@ -106,10 +106,102 @@ func resourceNewRelicDashboard() *schema.Resource {
 							Type:     schema.TypeString,
 							Optional: true,
 						},
-						// TODO: Move this to a set/map?
 						"nrql": {
 							Type:     schema.TypeString,
 							Optional: true,
+						},
+						"source": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"duration": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Default:      0,
+							ValidateFunc: validation.IntAtLeast(1),
+						},
+						"end_time": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Default:      0,
+							ValidateFunc: validation.IntAtLeast(1),
+						},
+						"raw_metric_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"facet": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"order_by": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"limit": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Default:      0,
+							ValidateFunc: validation.IntAtLeast(1),
+						},
+						"entity_ids": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeInt},
+						},
+						"metric": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"units": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"scope": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"values": {
+										Type:     schema.TypeSet,
+										Optional: true,
+										Elem:     &schema.Schema{Type: schema.TypeString},
+									},
+								},
+							},
+						},
+						"compare_with": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"offset_duration": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"presentation": {
+										Type:     schema.TypeList,
+										Required: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"color": {
+													Type:     schema.TypeString,
+													Required: true,
+												},
+												"name": {
+													Type:     schema.TypeString,
+													Required: true,
+												},
+											},
+										},
+									},
+								},
+							},
 						},
 					},
 				},
@@ -118,150 +210,16 @@ func resourceNewRelicDashboard() *schema.Resource {
 	}
 }
 
-func resourceNewRelicDashboardWidgetsHash(v interface{}) int {
-	var buf bytes.Buffer
-	m := v.(map[string]interface{})
-
-	row := m["row"].(int)
-	column := m["column"].(int)
-	width := m["width"].(int)
-	height := m["height"].(int)
-	nrql := m["nrql"].(string)
-	title := m["title"].(string)
-	notes := m["notes"].(string)
-	viz := m["visualization"].(string)
-
-	buf.WriteString(fmt.Sprintf("%d-%d-%d-%d-%s-%s-%s-%s",
-		row, column, width, height, nrql, title, viz, notes))
-
-	return hashcode.String(buf.String())
-}
-
-// Assemble the *newrelic.Dashboard variable.
-//
-// Used by the newrelic_dashboard Create and Update functions.
-func expandDashboard(d *schema.ResourceData) *newrelic.Dashboard {
-	metadata := newrelic.DashboardMetadata{
-		Version: 1,
-	}
-
-	// TODO: Some of these should be terraform defaults and validated
-	dashboard := newrelic.Dashboard{
-		Title:      d.Get("title").(string),
-		Metadata:   metadata,
-		Icon:       d.Get("icon").(string),
-		Visibility: d.Get("visibility").(string),
-		Editable:   d.Get("editable").(string),
-	}
-
-	if f, ok := d.GetOk("filter"); ok {
-		filter := f.([]interface{})[0].(map[string]interface{})
-		dashboardFilter := newrelic.DashboardFilter{}
-
-		if v, ok := filter["attributes"]; ok {
-			attributes := v.(*schema.Set).List()
-			vs := make([]string, 0, len(attributes))
-			for _, a := range attributes {
-				vs = append(vs, a.(string))
-			}
-
-			dashboardFilter.Attributes = vs
-		}
-
-		if v, ok := filter["event_types"]; ok {
-			eventTypes := v.(*schema.Set).List()
-			vs := make([]string, 0, len(eventTypes))
-			for _, e := range eventTypes {
-				vs = append(vs, e.(string))
-			}
-			dashboardFilter.EventTypes = vs
-		}
-		dashboard.Filter = dashboardFilter
-	}
-
-	log.Printf("[INFO] widget schema: %+v\n", d.Get("widget"))
-
-	if w, ok := d.GetOk("widget"); ok {
-		widgets := w.(*schema.Set).List()
-		for _, widget := range widgets {
-			w := widget.(map[string]interface{})
-
-			widgetPresentation := newrelic.DashboardWidgetPresentation{
-				Title: w["title"].(string),
-				Notes: w["notes"].(string),
-			}
-
-			widgetLayout := newrelic.DashboardWidgetLayout{
-				Row:    w["row"].(int),
-				Column: w["column"].(int),
-				Width:  w["width"].(int),
-				Height: w["height"].(int),
-			}
-
-			// TODO: Support non-NRQL Widgets
-			widgetData := []newrelic.DashboardWidgetData{
-				{
-					NRQL: w["nrql"].(string),
-				},
-			}
-
-			dashboard.Widgets = append(dashboard.Widgets, newrelic.DashboardWidget{
-				Visualization: w["visualization"].(string),
-				Layout:        widgetLayout,
-				Presentation:  widgetPresentation,
-				Data:          widgetData,
-			})
-		}
-	}
-
-	return &dashboard
-}
-
-// Unpack the *newrelic.Dashboard variable and set resource data.
-//
-// Used by the newrelic_dashboard Read function (resourceNewRelicDashboardRead)
-func flattenDashboard(dashboard *newrelic.Dashboard, d *schema.ResourceData) error {
-	d.Set("title", dashboard.Title)
-	d.Set("icon", dashboard.Icon)
-	d.Set("visibility", dashboard.Visibility)
-	d.Set("editable", dashboard.Editable)
-	d.Set("dashboard_url", dashboard.UIURL)
-	if filterErr := d.Set("filter", flattenFilter(&dashboard.Filter)); filterErr != nil {
-		return filterErr
-	}
-	widgetSet := schema.Set{
-		F: resourceNewRelicDashboardWidgetsHash,
-	}
-	for _, widget := range dashboard.Widgets {
-		values := map[string]interface{}{}
-		values["visualization"] = widget.Visualization
-		values["title"] = widget.Presentation.Title
-		values["notes"] = widget.Presentation.Notes
-		values["row"] = widget.Layout.Row
-		values["column"] = widget.Layout.Column
-		values["width"] = widget.Layout.Width
-		values["height"] = widget.Layout.Height
-
-		// TODO: Support non-NRQL Widgets
-		if len(widget.Data) > 0 {
-			values["nrql"] = widget.Data[0].NRQL
-		}
-		widgetSet.Add(values)
-	}
-	err := d.Set("widget", &widgetSet)
+func resourceNewRelicDashboardCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ProviderConfig).Client
+	dashboard, err := expandDashboard(d)
 	if err != nil {
 		return err
 	}
 
-	return nil
-}
-
-func resourceNewRelicDashboardCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*ProviderConfig).Client
-	dashboard := expandDashboard(d)
 	log.Printf("[INFO] Creating New Relic dashboard: %s", dashboard.Title)
 
-	dashboard, err := client.CreateDashboard(*dashboard)
+	dashboard, err = client.CreateDashboard(*dashboard)
 	if err != nil {
 		return err
 	}
@@ -296,7 +254,10 @@ func resourceNewRelicDashboardRead(d *schema.ResourceData, meta interface{}) err
 
 func resourceNewRelicDashboardUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*ProviderConfig).Client
-	dashboard := expandDashboard(d)
+	dashboard, err := expandDashboard(d)
+	if err != nil {
+		return err
+	}
 
 	id, err := strconv.Atoi(d.Id())
 	if err != nil {
@@ -332,30 +293,4 @@ func resourceNewRelicDashboardDelete(d *schema.ResourceData, meta interface{}) e
 	}
 
 	return nil
-}
-
-func flattenFilter(f *newrelic.DashboardFilter) []interface{} {
-	if f == nil {
-		return nil
-	}
-
-	if len(f.Attributes) == 0 && len(f.EventTypes) == 0 {
-		return nil
-	}
-
-	filterResult := make(map[string]interface{})
-
-	attributesList := make([]interface{}, 0, len(f.Attributes))
-	for _, v := range f.Attributes {
-		attributesList = append(attributesList, v)
-	}
-
-	eventTypesList := make([]interface{}, 0, len(f.EventTypes))
-	for _, v := range f.EventTypes {
-		eventTypesList = append(eventTypesList, v)
-	}
-
-	filterResult["attributes"] = schema.NewSet(schema.HashString, attributesList)
-	filterResult["event_types"] = schema.NewSet(schema.HashString, eventTypesList)
-	return []interface{}{filterResult}
 }
