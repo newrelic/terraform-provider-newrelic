d345b7270f5b60c44e1247dbd6ea5f519801385b
Sander Blue <sblue@newrelic.com>
2019-11-22 11:40:04 -0800

    chore: test prefix should be TestAcc for this test case

--- newrelic/resource_newrelic_alert_policy_test.go
+++ newrelic/resource_newrelic_alert_policy_test.go
@@ -86,7 +86,7 @@ func TestAccNewRelicAlertPolicy_ResourceNotFound(t *testing.T) {
 	})
 }
 
-func TestNewRelicAlertPolicy_ErrorThrownWhenNameEmpty(t *testing.T) {
+func TestAccNewRelicAlertPolicy_ErrorThrownWhenNameEmpty(t *testing.T) {
 	expectedErrorMsg, _ := regexp.Compile(`name must not be empty`)
 
 	resource.ParallelTest(t, resource.TestCase{
