2025d3b0e5065da9897954611a05d8f004d54623
Sander Blue <sblue@newrelic.com>
2019-11-22 10:19:40 -0800

    feat: add import functionality for newrelic_alert_policy data source

--- newrelic/data_source_newrelic_alert_policy.go
+++ newrelic/data_source_newrelic_alert_policy.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	newrelic "github.com/paultyng/go-newrelic/v4/api"
@@ -13,7 +14,6 @@ import (
 func dataSourceNewRelicAlertPolicy() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceNewRelicAlertPolicyRead,
-
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
@@ -59,11 +59,17 @@ func dataSourceNewRelicAlertPolicyRead(d *schema.ResourceData, meta interface{})
 		return fmt.Errorf("the name '%s' does not match any New Relic alert policy", name)
 	}
 
+	// New Relic provides created_at and updated_at as millisecond unix timestamps
+	// https://www.terraform.io/docs/extend/schemas/schema-types.html#date-amp-time-data
+	// "TypeString is also used for date/time data, the preferred format is RFC 3339."
+	created := unixMillis(policy.CreatedAt).Format(time.RFC3339)
+	updated := unixMillis(policy.UpdatedAt).Format(time.RFC3339)
+
 	d.SetId(strconv.Itoa(policy.ID))
 	d.Set("name", policy.Name)
 	d.Set("incident_preference", policy.IncidentPreference)
-	d.Set("created_at", policy.CreatedAt)
-	d.Set("updated_at", policy.UpdatedAt)
+	d.Set("created_at", created)
+	d.Set("updated_at", updated)
 
 	return nil
 }
