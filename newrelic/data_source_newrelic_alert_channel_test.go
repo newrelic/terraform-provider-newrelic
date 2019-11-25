7ed0bf012522f7b8cbbea3cdd736fabbfe81401a
Sander Blue <sblue@newrelic.com>
2019-11-25 13:34:19 -0800

    chore(tests): update newrelic_alert_channel data source acceptance tests

--- newrelic/data_source_newrelic_alert_channel_test.go
+++ newrelic/data_source_newrelic_alert_channel_test.go
@@ -11,36 +11,30 @@ import (
 )
 
 func TestAccNewRelicAlertChannelDataSource_Basic(t *testing.T) {
+	resourceName := "newrelic_alert_channel.foo"
 	rName := acctest.RandString(5)
-	resource.Test(t, resource.TestCase{
+
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
+			// Test: Create
 			{
 				Config: testAccNewRelicAlertChannelDataSourceConfig(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccNewRelicAlertChannel("data.newrelic_alert_channel.channel"),
+					resource.TestCheckResourceAttr(resourceName, "name", fmt.Sprintf("tf-test-%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "type", "email"),
 				),
+			},
+			// Test: Import
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
-}
-
-func testAccNewRelicAlertChannel(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		r := s.RootModule().Resources[n]
-		a := r.Primary.Attributes
-
-		if a["id"] == "" {
-			return fmt.Errorf("expected to get an alert channel from New Relic")
-		}
-
-		if strings.Contains(strings.ToLower(testAccExpectedAlertChannelName), strings.ToLower(a["name"])) {
-			return fmt.Errorf("expected the alert channel name to be: %s, but got: %s", testAccExpectedAlertChannelName, a["name"])
-		}
-
-		return nil
-	}
 }
 
 func testAccNewRelicAlertChannelDataSourceConfig(rName string) string {
@@ -59,4 +53,21 @@ data "newrelic_alert_channel" "channel" {
 	name = "${newrelic_alert_channel.foo.name}"
 }
 `, rName)
+}
+
+func testAccNewRelicAlertChannel(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		r := s.RootModule().Resources[n]
+		a := r.Primary.Attributes
+
+		if a["id"] == "" {
+			return fmt.Errorf("expected to get an alert channel from New Relic")
+		}
+
+		if strings.Contains(strings.ToLower(testAccExpectedAlertChannelName), strings.ToLower(a["name"])) {
+			return fmt.Errorf("expected the alert channel name to be: %s, but got: %s", testAccExpectedAlertChannelName, a["name"])
+		}
+
+		return nil
+	}
 }
