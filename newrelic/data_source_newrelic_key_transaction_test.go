292fc93415e14a8a46dca070b85e9ac825c88dce
Chris Trombley <ctrombley@newrelic.com>
2019-12-03 16:31:42 -0800

    test: add an acceptance test for key transactions data source

---
+++ newrelic/data_source_newrelic_key_transaction_test.go
@@ -0,0 +1,61 @@
+package newrelic
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+var (
+	testAccExpectedKeyTransactionName string = "get /"
+)
+
+func TestAccNewRelicKeyTransactionDataSource_Basic(t *testing.T) {
+	if !nrInternalAccount {
+		t.Skipf("New Relic internal testing account required")
+	}
+
+	resourceName := "data.newrelic_key_transaction.foo"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicKeyTransactionDataSourceConfig(testAccExpectedKeyTransactionName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicKeyTransactionDataSourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", testAccExpectedKeyTransactionName),
+				),
+			},
+		},
+	})
+}
+
+func testAccNewRelicKeyTransactionDataSourceConfig(rName string) string {
+	return fmt.Sprintf(`
+data "newrelic_key_transaction" "foo" {
+	name = "%s"
+}
+`, testAccExpectedKeyTransactionName)
+}
+
+func testAccCheckNewRelicKeyTransactionDataSourceExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		r := s.RootModule().Resources[n]
+		a := r.Primary.Attributes
+
+		if a["id"] == "" {
+			return fmt.Errorf("expected to get a key transaction from New Relic")
+		}
+
+		if !strings.EqualFold(testAccExpectedKeyTransactionName, a["name"]) {
+			return fmt.Errorf("expected the key transaction name to be: %s, but got: %s", testAccExpectedKeyTransactionName, a["name"])
+		}
+
+		return nil
+	}
+}
