aef8e75a29e52bd8a9b1ceac3bc74c1e385ee531
Chris Trombley <ctrombley@newrelic.com>
2019-12-20 11:17:58 -0800

    refactor: update Epoch to EpochTime

--- internal/serialization/types.go
+++ internal/serialization/types.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
-// Epoch is a type used for unmarshaling timestamps represented in epoch time.
+// EpochTime is a type used for unmarshaling timestamps represented in epoch time.
 // Its underlying type is time.Time.
-type Epoch time.Time
+type EpochTime time.Time
 
-// MarshalJSON is responsible for marshaling the Epoch type.
-func (e Epoch) MarshalJSON() ([]byte, error) {
+// MarshalJSON is responsible for marshaling the EpochTime type.
+func (e EpochTime) MarshalJSON() ([]byte, error) {
 	return []byte(strconv.FormatInt(time.Time(e).Unix(), 10)), nil
 }
 
-// UnmarshalJSON is responsible for unmarshaling the Epoch type.
-func (e *Epoch) UnmarshalJSON(s []byte) (err error) {
+// UnmarshalJSON is responsible for unmarshaling the EpochTime type.
+func (e *EpochTime) UnmarshalJSON(s []byte) (err error) {
 	q, err := strconv.ParseInt(string(s), 10, 64)
 	if err != nil {
 		return err
@@ -24,7 +24,7 @@ func (e *Epoch) UnmarshalJSON(s []byte) (err error) {
 	return
 }
 
-// Equal provides a comparator for the Epoch type.
-func (e Epoch) Equal(u Epoch) bool {
+// Equal provides a comparator for the EpochTime type.
+func (e EpochTime) Equal(u EpochTime) bool {
 	return time.Time(e).Equal(time.Time(u))
 }
--- pkg/alerts/alert_policies_types.go
+++ pkg/alerts/alert_policies_types.go
@@ -4,9 +4,9 @@ import "github.com/newrelic/newrelic-client-go/internal/serialization"
 
 // AlertPolicy represents a New Relic alert policy.
 type AlertPolicy struct {
-	ID                 int                  `json:"id,omitempty"`
-	IncidentPreference string               `json:"incident_preference,omitempty"`
-	Name               string               `json:"name,omitempty"`
-	CreatedAt          *serialization.Epoch `json:"created_at,omitempty"`
-	UpdatedAt          *serialization.Epoch `json:"updated_at,omitempty"`
+	ID                 int                      `json:"id,omitempty"`
+	IncidentPreference string                   `json:"incident_preference,omitempty"`
+	Name               string                   `json:"name,omitempty"`
+	CreatedAt          *serialization.EpochTime `json:"created_at,omitempty"`
+	UpdatedAt          *serialization.EpochTime `json:"updated_at,omitempty"`
 }
