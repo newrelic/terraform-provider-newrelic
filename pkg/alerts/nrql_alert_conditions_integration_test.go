fc4f24030ad6939f927ab35d9c0bb3dcace344fc
Sander Blue <sblue@newrelic.com>
2019-12-26 20:03:13 -0800

    tests: add nrql alert condition integration test

--- pkg/alerts/nrql_alert_conditions_integration_test.go
+++ pkg/alerts/nrql_alert_conditions_integration_test.go
@@ -1,3 +1,96 @@
 // +build integration
 
 package alerts
+
+import (
+	"fmt"
+	"testing"
+
+	nr "github.com/newrelic/newrelic-client-go/internal/testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegrationNrqlAlertConditions(t *testing.T) {
+	t.Parallel()
+
+	var (
+		randomString = nr.RandSeq(5)
+		alertPolicy  = AlertPolicy{
+			Name:               fmt.Sprintf("test-integration-nrql-policy-%s", randomString),
+			IncidentPreference: "PER_POLICY",
+		}
+		nrqlConditionName        = fmt.Sprintf("test-integration-nrql-condition-%s", randomString)
+		nrqlConditionNameUpdated = fmt.Sprintf("test-integration-nrql-condition-updated-%s", randomString)
+		nrqlCondition            = NrqlCondition{
+			Nrql: NrqlQuery{
+				Query:      "SELECT count(*) FROM Transactions",
+				SinceValue: "3",
+			},
+			Terms: []AlertConditionTerm{
+				{
+					Duration:     5,
+					Operator:     "above",
+					Priority:     "critical",
+					Threshold:    1,
+					TimeFunction: "all",
+				},
+			},
+			Type:                "static",
+			Name:                nrqlConditionName,
+			RunbookURL:          "https://www.example.com",
+			ValueFunction:       "single_value",
+			ViolationCloseTimer: 3600,
+			Enabled:             true,
+		}
+	)
+
+	client := newIntegrationTestClient(t)
+
+	// Setup
+	policy, err := client.CreateAlertPolicy(alertPolicy)
+
+	require.NoError(t, err)
+
+	nrqlCondition.PolicyID = policy.ID
+
+	// Deferred teardown
+	defer func() {
+		_, err := client.DeleteAlertPolicy(policy.ID)
+
+		if err != nil {
+			t.Logf("error cleaning up alert policy %d (%s): %s", policy.ID, policy.Name, err)
+		}
+	}()
+
+	// Test: Create
+	createResult, err := client.CreateNrqlAlertCondition(nrqlCondition)
+
+	require.NoError(t, err)
+	require.NotNil(t, createResult)
+
+	// Test: List
+	listResult, err := client.ListNrqlAlertConditions(createResult.PolicyID)
+
+	require.NoError(t, err)
+	require.Greater(t, len(listResult), 0)
+
+	// Test: Get
+	readResult, err := client.GetNrqlAlertCondition(createResult.PolicyID, createResult.ID)
+
+	require.NoError(t, err)
+	require.NotNil(t, readResult)
+
+	// Test: Update
+	createResult.Name = nrqlConditionNameUpdated
+	updateResult, err := client.UpdateNrqlAlertCondition(*createResult)
+
+	require.NoError(t, err)
+	require.NotNil(t, updateResult)
+	require.Equal(t, nrqlConditionNameUpdated, updateResult.Name)
+
+	// Test: Delete
+	result, err := client.DeleteNrqlAlertCondition(updateResult.ID)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+}
