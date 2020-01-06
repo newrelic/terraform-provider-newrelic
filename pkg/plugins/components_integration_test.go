9d95b54d77398c22bc2309e06b83e04194881580
Chris Trombley <ctrombley@newrelic.com>
2020-01-06 14:52:21 -0800

    refactor: simplify integration test scenarios for components

--- pkg/plugins/components_integration_test.go
+++ pkg/plugins/components_integration_test.go
@@ -10,25 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestIntegrationListComponents(t *testing.T) {
-	t.Parallel()
-
-	apiKey := os.Getenv("NEWRELIC_API_KEY")
-
-	if apiKey == "" {
-		t.Skipf("acceptance testing requires an API key")
-	}
-
-	api := New(config.Config{
-		APIKey: apiKey,
-	})
-
-	_, err := api.ListComponents(nil)
-
-	require.NoError(t, err)
-}
-
-func TestIntegrationGetComponent(t *testing.T) {
+func TestIntegrationComponents(t *testing.T) {
 	t.Parallel()
 
 	apiKey := os.Getenv("NEWRELIC_API_KEY")
