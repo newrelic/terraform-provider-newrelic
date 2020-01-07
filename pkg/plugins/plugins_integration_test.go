c4f8449a196b5aa0a59ef26d93de7f396359d4d6
Sander Blue <sblue@newrelic.com>
2020-01-06 16:34:58 -0800

    tests: add plugins integration test

--- pkg/plugins/plugins_integration_test.go
+++ pkg/plugins/plugins_integration_test.go
@@ -1,3 +1,35 @@
 // +build integration
 
 package plugins
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegrationPlugins(t *testing.T) {
+	t.Parallel()
+
+	client := newIntegrationTestClient(t)
+
+	// Test: List
+	listResult, err := client.ListPlugins(nil)
+
+	require.NoError(t, err)
+
+	if len(listResult) == 0 {
+		t.Skip("Skipping `GetPlugin()` integration test due to zero plugins found")
+		return
+	}
+
+	// Test: Get
+	qp := GetPluginParams{
+		Detailed: true,
+	}
+	getResult, err := client.GetPlugin(listResult[0].ID, &qp)
+
+	require.NoError(t, err)
+	require.NotNil(t, getResult)
+	require.NotNil(t, getResult.Details)
+}
