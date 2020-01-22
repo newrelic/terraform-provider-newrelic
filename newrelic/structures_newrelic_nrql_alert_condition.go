a910c79d13066867a44c6542a261ee629655543f
Sander Blue <sblue@newrelic.com>
2020-01-22 15:15:39 -0800

    refactor(alerts): update resource_newrelic_nrql_alert_condition to use new client (#348)

--- newrelic/structures_newrelic_nrql_alert_condition.go
+++ newrelic/structures_newrelic_nrql_alert_condition.go
@@ -1,14 +1,132 @@
 package newrelic
 
 import (
-	newrelic "github.com/paultyng/go-newrelic/v4/api"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/newrelic/newrelic-client-go/pkg/alerts"
 )
 
-func flattenNrql(nrql newrelic.AlertNrqlQuery) []interface{} {
+func expandNrqlAlertConditionStruct(d *schema.ResourceData) *alerts.NrqlCondition {
+	termSet := d.Get("term").([]interface{})
+	terms := expandNrqlConditionTerms(termSet)
+
+	query := alerts.NrqlQuery{}
+
+	if nrqlQuery, ok := d.GetOk("nrql.0.query"); ok {
+		query.Query = nrqlQuery.(string)
+	}
+
+	if sinceValue, ok := d.GetOk("nrql.0.since_value"); ok {
+		query.SinceValue = sinceValue.(string)
+	}
+
+	condition := alerts.NrqlCondition{
+		Name:                d.Get("name").(string),
+		Type:                d.Get("type").(string),
+		Enabled:             d.Get("enabled").(bool),
+		Terms:               terms,
+		PolicyID:            d.Get("policy_id").(int),
+		Nrql:                query,
+		ValueFunction:       d.Get("value_function").(string),
+		ViolationCloseTimer: d.Get("violation_time_limit_seconds").(int),
+	}
+
+	if attr, ok := d.GetOk("runbook_url"); ok {
+		condition.RunbookURL = attr.(string)
+	}
+
+	if attr, ok := d.GetOkExists("ignore_overlap"); ok {
+		condition.IgnoreOverlap = attr.(bool)
+	}
+
+	if attr, ok := d.GetOkExists("violation_time_limit_seconds"); ok {
+		condition.ViolationCloseTimer = attr.(int)
+	}
+
+	if attr, ok := d.GetOk("expected_groups"); ok {
+		condition.ExpectedGroups = attr.(int)
+	}
+
+	return &condition
+}
+
+func expandNrqlConditionTerms(terms []interface{}) []alerts.ConditionTerm {
+	trms := make([]alerts.ConditionTerm, len(terms))
+
+	for i, t := range terms {
+		term := t.(map[string]interface{})
+
+		trms[i] = alerts.ConditionTerm{
+			Duration:     term["duration"].(int),
+			Operator:     term["operator"].(string),
+			Priority:     term["priority"].(string),
+			Threshold:    term["threshold"].(float64),
+			TimeFunction: term["time_function"].(string),
+		}
+	}
+
+	return trms
+}
+
+func flattenNrqlQuery(nrql alerts.NrqlQuery) []interface{} {
 	m := map[string]interface{}{
 		"query":       nrql.Query,
 		"since_value": nrql.SinceValue,
 	}
 
 	return []interface{}{m}
+}
+
+func flattenNrqlConditionTerms(terms []alerts.ConditionTerm) []map[string]interface{} {
+	var t []map[string]interface{}
+
+	for _, src := range terms {
+		dst := map[string]interface{}{
+			"duration":      src.Duration,
+			"operator":      src.Operator,
+			"priority":      src.Priority,
+			"threshold":     src.Threshold,
+			"time_function": src.TimeFunction,
+		}
+		t = append(t, dst)
+	}
+
+	return t
+}
+
+func flattenNrqlConditionStruct(condition *alerts.NrqlCondition, d *schema.ResourceData) error {
+	ids, err := parseIDs(d.Id(), 2)
+	if err != nil {
+		return err
+	}
+
+	policyID := ids[0]
+
+	d.Set("policy_id", policyID)
+	d.Set("name", condition.Name)
+	d.Set("runbook_url", condition.RunbookURL)
+	d.Set("enabled", condition.Enabled)
+	d.Set("type", condition.Type)
+	d.Set("expected_groups", condition.ExpectedGroups)
+	d.Set("ignore_overlap", condition.IgnoreOverlap)
+	d.Set("violation_time_limit_seconds", condition.ViolationCloseTimer)
+
+	if condition.ValueFunction == "" {
+		d.Set("value_function", "single_value")
+	} else {
+		d.Set("value_function", condition.ValueFunction)
+	}
+
+	if err := d.Set("nrql", flattenNrqlQuery(condition.Nrql)); err != nil {
+		return err
+	}
+
+	terms := flattenNrqlConditionTerms(condition.Terms)
+
+	if err := d.Set("term", terms); err != nil {
+		return fmt.Errorf("[DEBUG] Error setting alert condition terms: %#v", err)
+	}
+
+	return nil
 }
--- newrelic/structures_newrelic_nrql_alert_condition_test.go
+++ newrelic/structures_newrelic_nrql_alert_condition_test.go
@@ -1,30 +1,79 @@
 package newrelic
 
 import (
-	"reflect"
 	"testing"
 
-	newrelic "github.com/paultyng/go-newrelic/v4/api"
+	"github.com/newrelic/newrelic-client-go/pkg/alerts"
+	"github.com/stretchr/testify/require"
 )
 
+func TestExpandNrqlConditionTerms(t *testing.T) {
+	flattened := []interface{}{
+		map[string]interface{}{
+			"duration":      5,
+			"operator":      "above",
+			"priority":      "critical",
+			"threshold":     1.5,
+			"time_function": "all",
+		},
+	}
+
+	expected := []alerts.ConditionTerm{
+		{
+			Duration:     5,
+			Operator:     "above",
+			Priority:     "critical",
+			Threshold:    1.5,
+			TimeFunction: "all",
+		},
+	}
+
+	expanded := expandNrqlConditionTerms(flattened)
+
+	require.NotNil(t, expanded)
+	require.Equal(t, expected, expanded)
+}
+
 func TestFlattenNrql(t *testing.T) {
-	expanded := newrelic.AlertNrqlQuery{
+	expanded := alerts.NrqlQuery{
 		Query:      "SELECT percentile(duration, 99) FROM Transaction FACET remote_ip",
 		SinceValue: "3",
 	}
 
-	flattened := []interface{}{map[string]interface{}{
+	expected := []interface{}{map[string]interface{}{
 		"query":       "SELECT percentile(duration, 99) FROM Transaction FACET remote_ip",
 		"since_value": "3",
 	}}
 
-	result := flattenNrql(expanded)
+	flattened := flattenNrqlQuery(expanded)
 
-	if result == nil {
-		t.Fatal("result was nil")
+	require.NotNil(t, flattened)
+	require.Equal(t, expected, flattened)
+}
+
+func TestFlattenNrqlConditionTerms(t *testing.T) {
+	expanded := []alerts.ConditionTerm{
+		{
+			Duration:     5,
+			Operator:     "above",
+			Priority:     "critical",
+			Threshold:    1.5,
+			TimeFunction: "all",
+		},
 	}
 
-	if !reflect.DeepEqual(result, flattened) {
-		t.Fatalf("result %s not equal to expected %s", result, flattened)
+	expected := []map[string]interface{}{
+		{
+			"duration":      5,
+			"operator":      "above",
+			"priority":      "critical",
+			"threshold":     1.5,
+			"time_function": "all",
+		},
 	}
+
+	flattened := flattenNrqlConditionTerms(expanded)
+
+	require.NotNil(t, flattened)
+	require.Equal(t, expected, flattened)
 }
