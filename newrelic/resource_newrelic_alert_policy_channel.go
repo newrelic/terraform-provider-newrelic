17742817dab44d04b28e156a12a795b0345b109b
Chris Trombley <ctrombley@newrelic.com>
2020-01-22 16:03:12 -0800

    refactor: rebase alerts policy channels on newrelic-client-go

--- newrelic/resource_newrelic_alert_policy_channel.go
+++ newrelic/resource_newrelic_alert_policy_channel.go
@@ -4,27 +4,9 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	newrelic "github.com/paultyng/go-newrelic/v4/api"
+	"github.com/newrelic/newrelic-client-go/newrelic"
+	"github.com/newrelic/newrelic-client-go/pkg/errors"
 )
-
-func policyChannelExists(client *newrelic.Client, policyID int, channelID int) (bool, error) {
-	channel, err := client.GetAlertChannel(channelID)
-	if err != nil {
-		if err == newrelic.ErrNotFound {
-			return false, nil
-		}
-
-		return false, err
-	}
-
-	for _, id := range channel.Links.PolicyIDs {
-		if id == policyID {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
 
 func resourceNewRelicAlertPolicyChannel() *schema.Resource {
 	return &schema.Resource{
@@ -51,7 +33,7 @@ func resourceNewRelicAlertPolicyChannel() *schema.Resource {
 }
 
 func resourceNewRelicAlertPolicyChannelCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*ProviderConfig).Client
+	client := meta.(*ProviderConfig).NewClient
 
 	policyID := d.Get("policy_id").(int)
 	channelID := d.Get("channel_id").(int)
@@ -66,7 +48,7 @@ func resourceNewRelicAlertPolicyChannelCreate(d *schema.ResourceData, meta inter
 	}
 
 	if !exists {
-		err = client.UpdateAlertPolicyChannels(policyID, []int{channelID})
+		_, err = client.Alerts.UpdatePolicyChannels(policyID, []int{channelID})
 		if err != nil {
 			return err
 		}
@@ -78,7 +60,7 @@ func resourceNewRelicAlertPolicyChannelCreate(d *schema.ResourceData, meta inter
 }
 
 func resourceNewRelicAlertPolicyChannelRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*ProviderConfig).Client
+	client := meta.(*ProviderConfig).NewClient
 
 	ids, err := parseIDs(d.Id(), 2)
 	if err != nil {
@@ -107,7 +89,7 @@ func resourceNewRelicAlertPolicyChannelRead(d *schema.ResourceData, meta interfa
 }
 
 func resourceNewRelicAlertPolicyChannelDelete(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*ProviderConfig).Client
+	client := meta.(*ProviderConfig).NewClient
 
 	ids, err := parseIDs(d.Id(), 2)
 	if err != nil {
@@ -125,9 +107,8 @@ func resourceNewRelicAlertPolicyChannelDelete(d *schema.ResourceData, meta inter
 	}
 
 	if exists {
-		if err := client.DeleteAlertPolicyChannel(policyID, channelID); err != nil {
-			switch err {
-			case newrelic.ErrNotFound:
+		if _, err := client.Alerts.DeletePolicyChannel(policyID, channelID); err != nil {
+			if _, ok := err.(*errors.NotFound); ok {
 				return nil
 			}
 			return err
@@ -135,4 +116,23 @@ func resourceNewRelicAlertPolicyChannelDelete(d *schema.ResourceData, meta inter
 	}
 
 	return nil
+}
+
+func policyChannelExists(client *newrelic.NewRelic, policyID int, channelID int) (bool, error) {
+	channel, err := client.Alerts.GetChannel(channelID)
+	if err != nil {
+		if _, ok := err.(*errors.NotFound); ok {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	for _, id := range channel.Links.PolicyIDs {
+		if id == policyID {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
