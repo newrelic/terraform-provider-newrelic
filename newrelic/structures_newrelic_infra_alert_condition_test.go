276f20cdad26fd4626e3b90e8396bd51ef62c38f
Sander Blue <sblue@newrelic.com>
2020-02-05 15:26:05 -0800

    feat(infra_alert_condition): add violation_close_timer to newrelic_infra_alert_condition resource (#370)

---
+++ newrelic/structures_newrelic_infra_alert_condition_test.go
@@ -0,0 +1,50 @@
+package newrelic
+
+import (
+	"testing"
+
+	"github.com/newrelic/newrelic-client-go/pkg/alerts"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExpandInfraAlertThreshold(t *testing.T) {
+	flattened := []interface{}{
+		map[string]interface{}{
+			"duration":      5,
+			"value":         1.5,
+			"time_function": "all",
+		},
+	}
+
+	expected := alerts.InfrastructureConditionThreshold{
+		Duration: 5,
+		Function: "all",
+		Value:    1.5,
+	}
+
+	expanded := expandInfraAlertThreshold(flattened)
+
+	require.NotNil(t, expanded)
+	require.Equal(t, &expected, expanded)
+}
+
+func TestFlattenInfraAlertThreshold(t *testing.T) {
+	expanded := alerts.InfrastructureConditionThreshold{
+		Duration: 5,
+		Function: "all",
+		Value:    1.5,
+	}
+
+	expected := []interface{}{
+		map[string]interface{}{
+			"duration":      5,
+			"value":         1.5,
+			"time_function": "all",
+		},
+	}
+
+	flattened := flattenAlertThreshold(&expanded)
+
+	require.NotNil(t, flattened)
+	require.Equal(t, expected, flattened)
+}
