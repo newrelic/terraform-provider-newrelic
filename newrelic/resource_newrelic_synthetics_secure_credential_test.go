1b9b1a87f690c16e6b844401b5696e73556004e1
Chris Trombley <ctrombley@newrelic.com>
2020-02-19 15:19:11 -0800

    feat(synthetics): add secure credentials resource (#409)
    * feat(synthetics): add secure credentials resource
    
    * feat(synthetics) add secure credential data source
    
    * docs(synthetics): document synthetics secure credentials resource and data source
    
    * chore(synthetics): linting and refactoring

---
+++ newrelic/resource_newrelic_synthetics_secure_credential_test.go
@@ -0,0 +1,109 @@
+package newrelic
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccNewRelicSyntheticsSecureCredential_Basic(t *testing.T) {
+	resourceName := "newrelic_synthetics_secure_credential.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicSyntheticsSecureCredentialDestroy,
+		Steps: []resource.TestStep{
+			// Test: Create
+			{
+				Config: testAccNewRelicSyntheticsSecureCredentialConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsSecureCredentialExists(resourceName),
+				),
+			},
+			// Test: Update
+			{
+				Config: testAccNewRelicSyntheticsSecureCredentialConfigUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsSecureCredentialExists(resourceName),
+				),
+			},
+			// Test: Import
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					// not returned from the API
+					"value",
+				},
+			},
+		},
+	})
+}
+
+func testAccCheckNewRelicSyntheticsSecureCredentialExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no synthetics secure credential key is set")
+		}
+
+		client := testAccProvider.Meta().(*ProviderConfig).NewClient
+
+		found, err := client.Synthetics.GetSecureCredential(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if !strings.EqualFold(found.Key, rs.Primary.ID) {
+			return fmt.Errorf("synthetics secure credential not found: %v - %v", rs.Primary.ID, found)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckNewRelicSyntheticsSecureCredentialDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*ProviderConfig).NewClient
+	for _, r := range s.RootModule().Resources {
+		if r.Type != "newrelic_synthetics_secure_credential" {
+			continue
+		}
+
+		_, err := client.Synthetics.GetSecureCredential(r.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("synthetics secure credential still exists")
+		}
+
+	}
+	return nil
+}
+
+func testAccNewRelicSyntheticsSecureCredentialConfig(name string) string {
+	return fmt.Sprintf(`
+resource "newrelic_synthetics_secure_credential" "foo" {
+	key          = "tf_test_%[1]s"
+	value        = "Test Value"
+	description  = "Test Description"
+}
+`, name)
+}
+
+func testAccNewRelicSyntheticsSecureCredentialConfigUpdated(name string) string {
+	return fmt.Sprintf(`
+resource "newrelic_synthetics_secure_credential" "foo" {
+	key         = "tf_test_%[1]s"
+	value        = "Test Value Updated"
+	description  = "Test Description"
+}
+`, name)
+}
---
+++ newrelic/structures_newrelic_synthetics_secure_credential.go
@@ -0,0 +1,35 @@
+package newrelic
+
+import (
+	"strings"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/newrelic/newrelic-client-go/pkg/synthetics"
+)
+
+func expandSyntheticsSecureCredential(d *schema.ResourceData) *synthetics.SecureCredential {
+	key := d.Get("key").(string)
+	key = strings.ToUpper(key)
+
+	sc := synthetics.SecureCredential{
+		Key:         key,
+		Value:       d.Get("value").(string),
+		Description: d.Get("description").(string),
+	}
+
+	return &sc
+}
+
+func flattenSyntheticsSecureCredential(sc *synthetics.SecureCredential, d *schema.ResourceData) error {
+	d.Set("key", sc.Key)
+	d.Set("description", sc.Description)
+
+	createdAt := time.Time(*sc.CreatedAt).Format(time.RFC3339)
+	d.Set("created_at", createdAt)
+
+	lastUpdated := time.Time(*sc.CreatedAt).Format(time.RFC3339)
+	d.Set("last_updated", lastUpdated)
+
+	return nil
+}
