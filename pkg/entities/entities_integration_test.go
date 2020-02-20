2fd1f08a6d9921fd67273d15561f8c670144d2b7
Sander Blue <sblue@newrelic.com>
2020-02-19 17:09:30 -0800

    fix(entities): tags filter needs to use type TagValue in graphql query

--- pkg/entities/entities_integration_test.go
+++ pkg/entities/entities_integration_test.go
@@ -18,11 +18,31 @@ func TestIntegrationSearchEntities(t *testing.T) {
 	params := SearchEntitiesParams{
 		Name: "Dummy App",
 	}
+
 	actual, err := client.SearchEntities(params)
 
 	require.NoError(t, err)
 	require.Greater(t, len(actual), 0)
 }
+
+func TestIntegrationSearchEntitiesByTags(t *testing.T) {
+	t.Parallel()
+
+	client := newIntegrationTestClient(t)
+
+	params := SearchEntitiesParams{
+		Tags: &TagValue{
+			Key:   "language",
+			Value: "nodejs",
+		},
+	}
+
+	actual, err := client.SearchEntities(params)
+
+	require.NoError(t, err)
+	require.NotNil(t, actual)
+}
+
 func TestIntegrationGetEntities(t *testing.T) {
 	t.Parallel()
 
