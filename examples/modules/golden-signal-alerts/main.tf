36e759ef36740a6673242d83ec4c2a623b6a6aaa
Chris Trombley <ctrombley@newrelic.com>
2020-03-09 11:09:12 -0700

    docs(examples): add a golden signal alerting module example

---
+++ examples/modules/golden-signal-alerts/main.tf
@@ -0,0 +1,102 @@
+data "newrelic_application" "application" {
+	name = var.service.name
+}
+
+resource "newrelic_alert_policy" "golden_signal_policy" {
+	name = "Golden Signals - ${var.service.name}"
+}
+
+resource "newrelic_alert_condition" "response_time_web" {
+	policy_id = newrelic_alert_policy.golden_signal_policy.id
+
+	name            = "High Response Time (web)"
+	type            = "apm_app_metric"
+	entities        = [data.newrelic_application.application.id]
+	metric          = "response_time_web"
+	condition_scope = "application"
+
+	term {
+		duration      = var.service.duration
+		threshold     = var.service.response_time_threshold
+		operator      = "above"
+		priority      = "critical"
+		time_function = "all"
+	}
+}
+
+resource "newrelic_alert_condition" "throughput_web" {
+	policy_id = newrelic_alert_policy.golden_signal_policy.id
+
+	name            = "Low Throughput (web)"
+	type            = "apm_app_metric"
+	entities        = [data.newrelic_application.application.id]
+	metric          = "throughput_web"
+	condition_scope = "application"
+
+	term {
+		duration      = var.service.duration
+		threshold     = var.service.throughput_threshold
+		operator      = "below"
+		priority      = "critical"
+		time_function = "all"
+	}
+}
+
+resource "newrelic_alert_condition" "error_percentage" {
+	policy_id = newrelic_alert_policy.golden_signal_policy.id
+
+	name            = "High Error Percentage"
+	type            = "apm_app_metric"
+	entities        = [data.newrelic_application.application.id]
+	metric          = "error_percentage"
+	condition_scope = "application"
+
+	term {
+		duration      = var.service.duration
+		threshold     = var.service.error_percentage_threshold
+		operator      = "above"
+		priority      = "critical"
+		time_function = "all"
+	}
+}
+
+resource "newrelic_alert_condition" "apdex" {
+	policy_id = newrelic_alert_policy.golden_signal_policy.id
+
+	name            = "Apdex"
+	type            = "apm_app_metric"
+	entities        = [data.newrelic_application.application.id]
+	metric          = "apdex"
+	condition_scope = "application"
+
+	term {
+		duration      = var.service.duration
+		threshold     = var.service.apdex_threshold
+		operator      = "below"
+		priority      = "critical"
+		time_function = "all"
+	}
+}
+
+resource "newrelic_infra_alert_condition" "high_cpu" {
+	policy_id = newrelic_alert_policy.golden_signal_policy.id
+
+	name       = "High CPU usage"
+	type       = "infra_metric"
+	event      = "SystemSample"
+	select     = "cpuPercent"
+	comparison = "above"
+	where      = "(`applicationId` = '${data.newrelic_application.application.id}')"
+
+	critical {
+		duration      = var.service.duration
+		value         = var.service.cpu_threshold
+		time_function = "all"
+	}
+}
+
+resource "newrelic_alert_policy_channel" "alert_policy_channel" {
+	count = length(var.alert_channel_ids)
+	policy_id  = newrelic_alert_policy.golden_signal_policy.id
+	channel_id  = var.alert_channel_ids[count.index]
+}
---
+++ examples/modules/golden-signal-alerts/variables.tf
@@ -0,0 +1,17 @@
+variable "service" {
+	description = "The service to create alerts for"
+	type = object({
+		name                       = string
+		duration                   = number
+		cpu_threshold              = number
+		apdex_threshold            = number
+		response_time_threshold    = number
+		error_percentage_threshold = number
+		throughput_threshold       = number
+	})
+}
+
+variable "alert_channel_ids" {
+	description = "The notification channel IDs to add to this policy"
+	type        = list(number)
+}
