14e3c17db42ef6f6d395ab3d129ef66214c77f51
Jonathan Thurman <jthurman@newrelic.com>
2020-05-14 13:51:24 -0700

    chore(build): Integrate standard make system

---
+++ build/deps.mk
@@ -0,0 +1,23 @@
+#
+# Makefile fragment for installing deps
+#
+
+GO           ?= go
+
+deps: tools deps-only
+
+deps-only:
+	@echo "=== $(PROJECT_NAME) === [ deps             ]: Installing package dependencies required by the project..."
+	@$(GO) mod tidy
+	@$(GO) mod download
+
+tools: check-version
+	@echo "=== $(PROJECT_NAME) === [ tools            ]: Installing tools required by the project..."
+	@$(GO) install $(GOTOOLS)
+
+tools-update: check-version
+	@echo "=== $(PROJECT_NAME) === [ tools-update     ]: Updating tools required by the project..."
+	@$(GO) get -u $(GOTOOLS)
+
+
+.PHONY: deps deps-only tools tools-update
