5f6c25d1cb83b594807411f7de04a45e2e998150
Zach Leslie <zleslie@newrelic.com>
2020-06-03 10:57:01 -0700

    test(alerts): include tests for expandNrqlAlertConditionInput()

--- newrelic/structures_newrelic_nrql_alert_condition_test.go
+++ newrelic/structures_newrelic_nrql_alert_condition_test.go
@@ -77,3 +77,118 @@ func TestFlattenNrqlConditionTerms(t *testing.T) {
 	require.NotNil(t, flattened)
 	require.Equal(t, expected, flattened)
 }
+
+func TestExpandNrqlAlertConditionInput(t *testing.T) {
+
+	nrql := map[string]interface{}{
+		"query":             "SELECT percentile(duration, 95) FROM Transaction WHERE appName = 'Dummy App'",
+		"evaluation_offset": 3,
+	}
+
+	expectedNrql := &alerts.NrqlConditionInput{}
+	expectedNrql.Nrql.Query = nrql["query"].(string)
+	expectedNrql.Nrql.EvaluationOffset = nrql["evaluation_offset"].(int)
+
+	cases := map[string]struct {
+		Data         map[string]interface{}
+		ExpectErr    bool
+		ExpectReason string
+		Expanded     *alerts.NrqlConditionInput
+	}{
+		"invalid nrql": {
+			Data:         map[string]interface{}{},
+			ExpectErr:    true,
+			ExpectReason: "one of `since_value` or `evaluation_offset` must be configured for block `nrql`",
+		},
+		"valid nrql": {
+			Data: map[string]interface{}{
+				"nrql": []interface{}{nrql},
+			},
+			ExpectErr:    false,
+			ExpectReason: "",
+			Expanded:     expectedNrql,
+		},
+		"basline condition, requires baseline_direction attr": {
+			Data: map[string]interface{}{
+				"type": "baseline",
+			},
+			ExpectErr:    true,
+			ExpectReason: "attribute `baseline_direction` is required for nrql alert conditions of type `baseline`",
+		},
+		"basline condition, has baseline_direction attr": {
+			Data: map[string]interface{}{
+				"nrql":               []interface{}{nrql},
+				"type":               "baseline",
+				"baseline_direction": "lower_only",
+			},
+			ExpectErr:    false,
+			ExpectReason: "",
+			Expanded: &alerts.NrqlConditionInput{
+				BaselineDirection: &alerts.NrqlBaselineDirections.LowerOnly,
+			},
+		},
+		"static condition, requires value_function attr": {
+			Data: map[string]interface{}{
+				"nrql": []interface{}{nrql},
+				"type": "static",
+			},
+			ExpectErr:    true,
+			ExpectReason: "attribute `value_function` is required for nrql alert conditions of type `static`",
+		},
+		"static condition, has value_function attr": {
+			Data: map[string]interface{}{
+				"nrql":           []interface{}{nrql},
+				"type":           "static",
+				"value_function": "single_value",
+			},
+			ExpectErr:    false,
+			ExpectReason: "",
+			Expanded: &alerts.NrqlConditionInput{
+				ValueFunction: &alerts.NrqlConditionValueFunctions.SingleValue,
+			},
+		},
+	}
+
+	r := resourceNewRelicNrqlAlertCondition()
+
+	for _, tc := range cases {
+		d := r.TestResourceData()
+
+		for k, v := range tc.Data {
+			if err := d.Set(k, v); err != nil {
+				t.Fatalf("err: %s", err)
+			}
+		}
+
+		expanded, err := expandNrqlAlertConditionInput(d)
+
+		if tc.ExpectErr {
+			require.NotNil(t, err)
+			require.Equal(t, err.Error(), tc.ExpectReason)
+		} else {
+			require.Nil(t, err)
+		}
+
+		if tc.Expanded != nil {
+
+			// Static conditions specific
+			if tc.Expanded.ValueFunction != nil {
+				require.Equal(t, *tc.Expanded.ValueFunction, *expanded.ValueFunction)
+			}
+
+			// Baseline conditions specific
+			if tc.Expanded.BaselineDirection != nil {
+				require.Equal(t, *tc.Expanded.BaselineDirection, *expanded.BaselineDirection)
+			}
+
+			if tc.Expanded.Nrql.Query != "" {
+				require.Equal(t, tc.Expanded.Nrql.Query, expanded.Nrql.Query)
+			}
+
+			if tc.Expanded.Nrql.EvaluationOffset > 0 {
+				require.Equal(t, tc.Expanded.Nrql.EvaluationOffset, expanded.Nrql.EvaluationOffset)
+			}
+		}
+	}
+
+}
