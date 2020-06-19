78ffc90a10bdf9cfbb598b1b98fc6f69c8fd0174
Chris Trombley <ctrombley@newrelic.com>
2020-06-18 17:08:16 -0700

    chore: fix goreleaser reference

--- tools/tools.go
+++ tools/tools.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlint"
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/goreleaser"
 )
