6e25286042889ba9b1d263b0522f5b372297c056
Zach Leslie <zleslie@newrelic.com>
2020-06-22 16:00:09 -0700

    fix(alerts): improve nil handling for alert_channel
    resolves: #646

--- newrelic/structures_newrelic_alert_channel_test.go
+++ newrelic/structures_newrelic_alert_channel_test.go
@@ -39,6 +39,19 @@ func TestExpandAlertChannel(t *testing.T) {
 				},
 			},
 		},
+		"nil config": {
+			Data: map[string]interface{}{
+				"name":   "testing123",
+				"type":   "slack",
+				"config": []interface{}{nil},
+			},
+			Expanded: &alerts.Channel{
+				Name: "testing123",
+				Configuration: alerts.ChannelConfiguration{
+					URL: "https://example.com",
+				},
+			},
+		},
 	}
 
 	r := resourceNewRelicAlertChannel()
@@ -119,77 +132,66 @@ func TestFlattenAlertChannel(t *testing.T) {
 				}
 
 				if k == "config" {
-					for ck, cv := range v.(map[string]interface{}) {
+					testFlattenAlertChannelConfig(t, v, tc.Flattened.Configuration)
 
-						if ck == "api_key" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.APIKey)
-						}
-
-						if ck == "auth_password" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.AuthPassword)
-						}
-
-						if ck == "auth_username" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.AuthUsername)
-						}
-
-						if ck == "base_url" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.BaseURL)
-						}
-
-						if ck == "channel" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Channel)
-						}
-
-						if ck == "key" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Key)
-						}
-
-						if ck == "payload" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Payload)
-						}
-
-						if ck == "payload_type" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.PayloadType)
-						}
-
-						if ck == "recipients" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Recipients)
-						}
-
-						if ck == "region" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Region)
-						}
-
-						if ck == "route_key" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.RouteKey)
-						}
-
-						if ck == "service_key" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.ServiceKey)
-						}
-
-						if ck == "tags" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Tags)
-						}
-
-						if ck == "teams" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.Teams)
-						}
-
-						if ck == "url" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.URL)
-						}
-
-						if ck == "user_id" {
-							assert.Equal(t, cv, tc.Flattened.Configuration.UserID)
-						}
-
-					}
 				} else {
 					assert.Equal(t, x, v)
 				}
 			}
 		}
+	}
+}
+
+func testFlattenAlertChannelConfig(t *testing.T, v interface{}, config alerts.ChannelConfiguration) {
+	for ck, cv := range v.(map[string]interface{}) {
+		switch ck {
+		case "api_key":
+			assert.Equal(t, cv, config.APIKey)
+		case "auth_password":
+			assert.Equal(t, cv, config.AuthPassword)
+
+		case "auth_username":
+			assert.Equal(t, cv, config.AuthUsername)
+
+		case "base_url":
+			assert.Equal(t, cv, config.BaseURL)
+
+		case "channel":
+			assert.Equal(t, cv, config.Channel)
+
+		case "key":
+			assert.Equal(t, cv, config.Key)
+
+		case "payload":
+			assert.Equal(t, cv, config.Payload)
+
+		case "payload_type":
+			assert.Equal(t, cv, config.PayloadType)
+
+		case "recipients":
+			assert.Equal(t, cv, config.Recipients)
+
+		case "region":
+			assert.Equal(t, cv, config.Region)
+
+		case "route_key":
+			assert.Equal(t, cv, config.RouteKey)
+
+		case "service_key":
+			assert.Equal(t, cv, config.ServiceKey)
+
+		case "tags":
+			assert.Equal(t, cv, config.Tags)
+
+		case "teams":
+			assert.Equal(t, cv, config.Teams)
+
+		case "url":
+			assert.Equal(t, cv, config.URL)
+
+		case "user_id":
+			assert.Equal(t, cv, config.UserID)
+		}
+
 	}
 }
