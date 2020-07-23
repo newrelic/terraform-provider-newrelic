fb70184dbe541ca8509fd48e248d4e2b8a70c260
Chris Trombley <ctrombley@newrelic.com>
2020-07-23 12:39:56 -0700

    chore: only install build tools when running tests

--- build/test.mk
+++ build/test.mk
@@ -23,12 +23,12 @@ GOTOOLS += github.com/stretchr/testify/assert
 test: test-only
 test-only: test-unit test-integration
 
-test-unit: deps
+test-unit: tools
 	@echo "=== $(PROJECT_NAME) === [ test-unit        ]: running unit tests..."
 	@mkdir -p $(COVERAGE_DIR)
 	@$(TEST_RUNNER) -f testname --junitfile $(COVERAGE_DIR)/unit.xml -- -parallel 4 $(TEST_ARGS) -covermode=$(COVERMODE) -coverprofile $(COVERAGE_DIR)/unit.tmp $(GO_PKGS)
 
-test-integration: deps
+test-integration: tools
 	@echo "=== $(PROJECT_NAME) === [ test-integration ]: running integration tests..."
 	@mkdir -p $(COVERAGE_DIR)
 	TF_ACC=1 NR_ACC_TESTING=1 $(TEST_RUNNER) -f testname --junitfile $(COVERAGE_DIR)/integration.xml --rerun-fails=3 --packages $(GO_PKGS) -- -timeout 120m -v -ldflags=$(LDFLAGS_TEST) -parallel 4 $(TEST_ARGS) -covermode=$(COVERMODE) -coverprofile $(COVERAGE_DIR)/integration.tmp
