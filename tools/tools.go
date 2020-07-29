2c54b523ca7cdd2184c73fe13503710b7f2a0e04
Sander Blue <sblue@newrelic.com>
2020-07-29 13:26:50 -0700

    chore(chglog): use newrelic-forks/git-chglog to generate our CHANGELOG.md file

--- tools/tools.go
+++ tools/tools.go
@@ -5,8 +5,8 @@ package main
 import (
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlint"
 	_ "github.com/client9/misspell/cmd/misspell"
-	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/newrelic-forks/git-chglog/cmd/git-chglog"
 	_ "gotest.tools/gotestsum"
 )
