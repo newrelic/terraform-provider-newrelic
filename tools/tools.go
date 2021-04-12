04ce6a5c45cb879e47833b8e73e7ac5e31cbf304
Sander Blue <sblue@newrelic.com>
2021-04-12 16:18:07 -0500

    chore(build): update git-chglog now that --sort flag has been implemented

--- tools/tools.go
+++ tools/tools.go
@@ -5,10 +5,10 @@ package tools
 import (
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlint"
 	_ "github.com/client9/misspell/cmd/misspell"
+	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
 	_ "github.com/llorllale/go-gitlint/cmd/go-gitlint"
-	_ "github.com/newrelic-forks/git-chglog/cmd/git-chglog"
 	_ "github.com/psampaz/go-mod-outdated"
 	_ "github.com/stretchr/testify/assert"
 	_ "golang.org/x/tools/cmd/godoc"
