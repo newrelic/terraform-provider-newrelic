9eca0d31ccaebff926bb6a8697d2e4a018278d25
Maria Bazhlekova <mbazhlekova@newrelic.com>
2021-10-25 15:38:56 -0500

    chore: run go fmt

--- newrelic/resource_newrelic_alert_muting_rule.go
+++ newrelic/resource_newrelic_alert_muting_rule.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/newrelic/newrelic-client-go/pkg/errors"
-) 
+)
 
 func validateMutingRuleConditionAttribute(val interface{}, key string) (warns []string, errs []error) {
 	valueString := val.(string)
@@ -131,9 +131,9 @@ func resourceNewRelicAlertMutingRule() *schema.Resource {
 										Description:  "The attribute on a violation.",
 									},
 									"operator": {
-										Type:        schema.TypeString,
-										Required:    true,
-										Description: "The operator used to compare the attribute's value with the supplied value(s).",
+										Type:         schema.TypeString,
+										Required:     true,
+										Description:  "The operator used to compare the attribute's value with the supplied value(s).",
 										ValidateFunc: validation.StringInSlice([]string{"ANY", "CONTAINS", "ENDS_WITH", "EQUALS", "IN", "IS_BLANK", "IS_NOT_BLANK", "NOT_CONTAINS", "NOT_ENDS_WITH", "NOT_EQUALS", "NOT_IN", "NOT_STARTS_WITH", "STARTS_WITH"}, true),
 									},
 									"values": {
