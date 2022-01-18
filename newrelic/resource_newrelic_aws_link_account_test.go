21fc81f71d308c8ffb88f85c9ca74fea122c33b2
shivabandari <sbandari@newrelic.com>
2022-01-18 19:04:41 +0530

    chore: configurations not working

--- newrelic/resource_newrelic_aws_link_account_test.go
+++ newrelic/resource_newrelic_aws_link_account_test.go
@@ -6,27 +6,37 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccNewRelicAwsLinkAccount_Basic(t *testing.T) {
 	rName := acctest.RandString(5)
 
 	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccNewRelicAwsLinkAccountDestroy,
 		Steps: []resource.TestStep{
 			//Test: Create
 			{
-				Config: testAccNewRelicAwsAccountLinkAccountConfig(rName),
+				Config: testAccNewRelicAwsLinkAccountConfig(rName),
 			},
 			//Test: Update
 			//TODO
 			{
-				Config: testAccNewRelicAwsAccountLinkAccountConfigUpdated(rName),
+				Config: testAccNewRelicAwsLinkAccountConfigUpdated(rName),
 			},
 		},
 	})
 }
 
-func testAccNewRelicAwsAccountLinkAccountConfig(name string) string {
+func testAccNewRelicAwsLinkAccountDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*ProviderConfig).NewClient
+	for _, r := range s.RootModule().Resources {
+	}
+}
+
+func testAccNewRelicAwsLinkAccountConfig(name string) string {
 	return fmt.Sprintf(`
 	resource "newrelic_aws_link_account" "account" {
 		arn = ""
@@ -36,7 +46,7 @@ func testAccNewRelicAwsAccountLinkAccountConfig(name string) string {
 	`, name)
 }
 
-func testAccNewRelicAwsAccountLinkAccountConfigUpdated(name string) string {
+func testAccNewRelicAwsLinkAccountConfigUpdated(name string) string {
 	return fmt.Sprintf(`
 	resource "newrelic_aws_link_account" "account" {
 		arn = ""
