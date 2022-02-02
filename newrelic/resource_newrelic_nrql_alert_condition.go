fe3db2e4b02c0b273a501a6f177628abe14824dd
Jay McGrath <jaymcgrath@users.noreply.github.com>
2022-02-02 11:28:53 -0800

    fix(nrql_alert_condition): remove computed flag for slide_by field

--- newrelic/resource_newrelic_nrql_alert_condition.go
+++ newrelic/resource_newrelic_nrql_alert_condition.go
@@ -315,7 +315,6 @@ func resourceNewRelicNrqlAlertCondition() *schema.Resource {
 			"slide_by": {
 				Type:        schema.TypeInt,
 				Optional:    true,
-				Computed:    true,
 				Description: "The duration of overlapping timewindows used to smooth the chart line, in seconds. Must be a factor of `aggregation_window` and less than the aggregation window. It should be greater or equal to 30 seconds if `aggregation_window` is less than or equal to 3600 seconds, or greater or eqaul to `aggregation_window / 120` if `aggregation_window` is greater than 3600 seconds.",
 			},
 			"expiration_duration": {
