37c8da286ac0ee137cab4a1905ea6e22de0c1369
shivabandari <sbandari@newrelic.com>
2022-02-07 18:36:17 +0530

    chore: fixed code mistakes

--- newrelic/resource_newrelic_cloud_gcp_link_account_test.go
+++ newrelic/resource_newrelic_cloud_gcp_link_account_test.go
@@ -5,10 +5,11 @@ package newrelic
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"os"
 	"strconv"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
