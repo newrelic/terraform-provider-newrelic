00af316e5b3f3c3458fc594944156d45a99c5496
shivabandari <sbandari@newrelic.com>
2022-02-14 14:39:30 +0530

    chore: enabled stacker driver monitoring in gcp

--- newrelic/resource_newrelic_cloud_gcp_link_account_test.go
+++ newrelic/resource_newrelic_cloud_gcp_link_account_test.go
@@ -9,8 +9,6 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
