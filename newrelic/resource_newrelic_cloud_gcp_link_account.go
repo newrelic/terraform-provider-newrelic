c66e0efbc91bdc9f5c4ff4eb04ce4d1c4b69f98d
shivabandari <sbandari@newrelic.com>
2022-03-04 12:52:32 +0530

    chore: added changes as per the comments

--- newrelic/resource_newrelic_cloud_gcp_link_account.go
+++ newrelic/resource_newrelic_cloud_gcp_link_account.go
@@ -2,10 +2,7 @@ package newrelic
 
 import (
 	"context"
-	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -45,58 +42,53 @@ func resourceNewRelicCloudGcpLinkAccount() *schema.Resource {
 func resourceNewRelicCloudGcpLinkAccountCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	providerConfig := meta.(*ProviderConfig)
 	client := providerConfig.NewClient
+
 	accountID := selectAccountID(providerConfig, d)
+
 	linkAccountInput := expandGcpCloudLinkAccountInput(d)
+
+	cloudLinkAccountPayload, err := client.Cloud.CloudLinkAccountWithContext(ctx, accountID, linkAccountInput)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	var diags diag.Diagnostics
 
-	retryErr := resource.RetryContext(ctx, d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-
-		cloudLinkAccountPayload, err := client.Cloud.CloudLinkAccountWithContext(ctx, accountID, linkAccountInput)
-
-		if err != nil {
-			return resource.RetryableError(err)
+	if len(cloudLinkAccountPayload.Errors) > 0 {
+		for _, err := range cloudLinkAccountPayload.Errors {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  err.Type + " " + err.Message,
+			})
 		}
-
-		if len(cloudLinkAccountPayload.Errors) > 0 {
-			for _, err := range cloudLinkAccountPayload.Errors {
-				if strings.Contains(err.Message, "Validation failed: The account name you entered is not unique - please enter a new account name.") {
-					return resource.NonRetryableError(fmt.Errorf("%s : %s", err.Type, err.Message))
-				}
-				diags = append(diags, diag.Diagnostic{
-					Severity: diag.Error,
-					Summary:  err.Type + " " + err.Message,
-				})
-			}
-		}
-
-		d.SetId(strconv.Itoa(cloudLinkAccountPayload.LinkedAccounts[0].ID))
-
-		return nil
-	})
-
-	if retryErr != nil {
-		return diag.FromErr(retryErr)
-	}
-
-	if len(diags) > 0 {
 		return diags
 	}
+
+	//storing the linked account id after creating the linked account
+	d.SetId(strconv.Itoa(cloudLinkAccountPayload.LinkedAccounts[0].ID))
 
 	return nil
 }
 
+//Expand function to extract input variables from the schema.ResourceData
+//Here we extract name,project_id and account_id from the schema
 func expandGcpCloudLinkAccountInput(d *schema.ResourceData) cloud.CloudLinkCloudAccountsInput {
+
 	gcpAccount := cloud.CloudGcpLinkAccountInput{}
+
 	if name, ok := d.GetOk("name"); ok {
 		gcpAccount.Name = name.(string)
 	}
+
 	if projectID, ok := d.GetOk("project_id"); ok {
 		gcpAccount.ProjectId = projectID.(string)
 	}
+	//Here we add gcp inputs to cloudLinkAccountsInput
 	input := cloud.CloudLinkCloudAccountsInput{
 		Gcp: []cloud.CloudGcpLinkAccountInput{gcpAccount},
 	}
+
 	return input
 }
 
@@ -104,39 +96,55 @@ func resourceNewRelicCloudGcpLinkAccountRead(ctx context.Context, d *schema.Reso
 	providerConfig := meta.(*ProviderConfig)
 	client := providerConfig.NewClient
 	accountID := selectAccountID(providerConfig, d)
+
 	linkedAccountID, convErr := strconv.Atoi(d.Id())
+
 	if convErr != nil {
 		return diag.FromErr(convErr)
 	}
 	linkedAccount, err := client.Cloud.GetLinkedAccount(accountID, linkedAccountID)
+
 	if err != nil {
 		return diag.FromErr(err)
 	}
 	readGcpLinkedAccount(d, linkedAccount)
+
 	return nil
 }
 
+//To read the output and store these the output using d.Set function
+//Here we store name and project_id
 func readGcpLinkedAccount(d *schema.ResourceData, result *cloud.CloudLinkedAccount) {
+
 	_ = d.Set("name", result.Name)
 	_ = d.Set("project_id", result.ExternalId)
+
 }
 
 func resourceNewRelicCloudGcpLinkAccountUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	providerConfig := meta.(*ProviderConfig)
 	client := providerConfig.NewClient
 	accountID := selectAccountID(providerConfig, d)
+
+	//restoring the value of linked account id using the d.Id() function.
+	//Which we stored in create function
 	id, _ := strconv.Atoi(d.Id())
+
 	input := []cloud.CloudRenameAccountsInput{
 		{
 			Name:            d.Get("name").(string),
 			LinkedAccountId: id,
 		},
 	}
+
 	cloudRenameAccountPayload, err := client.Cloud.CloudRenameAccount(accountID, input)
+
 	if err != nil {
 		diag.FromErr(err)
 	}
+
 	var diags diag.Diagnostics
+
 	if len(cloudRenameAccountPayload.Errors) > 0 {
 		for _, err := range cloudRenameAccountPayload.Errors {
 			diags = append(diags, diag.Diagnostic{
@@ -150,23 +158,31 @@ func resourceNewRelicCloudGcpLinkAccountUpdate(ctx context.Context, d *schema.Re
 }
 
 func resourceNewRelicCloudGcpLinkAccountDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
 	providerConfig := meta.(*ProviderConfig)
 	client := providerConfig.NewClient
 	accountID := selectAccountID(providerConfig, d)
+
 	linkedAccountID, convErr := strconv.Atoi(d.Id())
+
 	if convErr != nil {
 		diag.FromErr(convErr)
 	}
+
 	unlinkAccountInput := []cloud.CloudUnlinkAccountsInput{
 		{
 			LinkedAccountId: linkedAccountID,
 		},
 	}
+
 	cloudUnlinkAccountPayload, err := client.Cloud.CloudUnlinkAccountWithContext(ctx, accountID, unlinkAccountInput)
+
 	if err != nil {
 		diag.FromErr(err)
 	}
+
 	var diags diag.Diagnostics
+
 	if len(cloudUnlinkAccountPayload.Errors) > 0 {
 		for _, err := range cloudUnlinkAccountPayload.Errors {
 			diags = append(diags, diag.Diagnostic{
@@ -176,6 +192,9 @@ func resourceNewRelicCloudGcpLinkAccountDelete(ctx context.Context, d *schema.Re
 		}
 		return diags
 	}
+
+	//setting the linked account id to null after deleting the resource
 	d.SetId("")
+
 	return nil
 }
