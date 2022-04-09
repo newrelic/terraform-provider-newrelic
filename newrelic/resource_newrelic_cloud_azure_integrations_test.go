b6d32aa07ed2bfac1df1938481b07c19a3c6cd11
shivabandari <sbandari@newrelic.com>
2022-04-09 14:23:40 +0530

    chore: minor changes

--- newrelic/resource_newrelic_cloud_azure_integrations_test.go
+++ newrelic/resource_newrelic_cloud_azure_integrations_test.go
@@ -49,7 +49,7 @@ func TestAccNewRelicCloudAzureLinkAccount_Basic(t *testing.T) {
 			{
 				Config: testAccNewRelicAzureIntegartionsConfig(testAzureApplicationID, testAzureClientSecretID, testAzureSubscriptionID, testAzureTenantID, randName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNewRelicCloudAzureIntegrationsAccountExist(resourceName),
+					testAccCheckNewRelicCloudAzureIntegrationsExist(resourceName),
 				),
 			},
 
@@ -57,7 +57,7 @@ func TestAccNewRelicCloudAzureLinkAccount_Basic(t *testing.T) {
 			{
 				Config: testAccNewRelicAzureIntegrationsConfigUpdated(testAzureApplicationID, testAzureClientSecretID, testAzureSubscriptionID, testAzureSubscriptionID, randName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNewRelicCloudAzureIntegrationsAccountExist(resourceName),
+					testAccCheckNewRelicCloudAzureIntegrationsExist(resourceName),
 				),
 			},
 			// Test: Import
@@ -127,352 +127,324 @@ func testAccCheckNewRelicCloudAzureIntegrationsDestroy(s *terraform.State) error
 
 func testAccNewRelicAzureIntegrationsConfig(applicationID string, clientSecretID string, subscriptionID string, tenantID string, name string) string {
 	return fmt.Sprintf(`
-	resource "newrelic_cloud_azure_link_account" "foo"{
+	resource "newrelic_cloud_azure_link_account" "foo1"{
 	account_id = 2520528
 	application_id = "%[1]s"
 	client_secret = "%[2]s"
 	subscription_id = "%[3]s"
 	tenant_id = "%[4]s"
 	name  = "%[5]s"
-}
+	}
 	
-resource "newrelic_cloud_azure_integrations" "foo" {
+	resource "newrelic_cloud_azure_integrations" "foo" {
 		linked_account_id = newrelic_cloud_azure_link_account.foo.id
 		account_id = 2520528
 		azure_api_management {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-				}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+		}
 		
 		azure_app_gateway {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-				}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+		}
 		azure_app_service"{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-				}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+		}
 		azure_containers {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
 				}
 					
 		azure_cosmos_db {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
 				}
 		azure_cost_management {
-						metrics_polling_interval = 1200
-						tag_keys = ""
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
 				}
 		
 		azure_data_factory {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		
 		azure_event_hub {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		
 		azure_express_route {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		azure_event_hub {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		azure_firewalls {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		azure_front_door {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		azure_functions {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		azure_key_vault {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200
+				resource_groups = "beyond"
+				}
 		azure_load_balancer { 
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
+				}
 		azure_logic_apps {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		azure_machine_learning {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		azure_maria_db {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		
 		azure_mysql {
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		azure_postgresql{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		azure_power_bi_dedicated{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
+				}
 		azure_power_bi_dedicated{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
-		
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
+				}
 		azure_redis_cache{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"	
+				}
 		
 		azure_service_bus{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
-		
-		}
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"	
+				}
 		
 		azure_sql{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		
 		azure_storage{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		
 		azure_virtual_machine{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		
 		azure_virtual_networks{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		
 		azure_vms{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 		
-		azure_vpn_gateway{
-						metrics_polling_interval = 1200
-						resource_groups = "beyond"
+		azure_vpn_gateway{	
+				metrics_polling_interval = 1200	
+				resource_groups = "beyond"
 		
-		}
+				}
 	`, applicationID, clientSecretID, subscriptionID, tenantID, name)
 
 }
 
 func testAccNewRelicAzureIntegrationsConfigUpdated(applicationID string, clientSecretID string, subscriptionID string, tenantID string, name string) string {
 	return fmt.Sprintf(`
-	resource "newrelic_cloud_azure_link_account" "foo"{
-		account_id = 2520528
-		application_id = "%[1]s"
-		client_secret = "%[2]s"
-		subscription_id = "%[3]s"
-		tenant_id = "%[4]s"
-		name  = "%[5]s"
+	resource "newrelic_cloud_azure_link_account" "foo1"{
+	account_id = 2520528
+	application_id = "%[1]s"
+	client_secret = "%[2]s"
+	subscription_id = "%[3]s"
+	tenant_id = "%[4]s"
+	name  = "%[5]s"
 	}
-
+	
 	resource "newrelic_cloud_azure_integrations" "foo" {
 		linked_account_id = newrelic_cloud_azure_link_account.foo.id
 		account_id = 2520528
 		azure_api_management {
-			metrics_polling_interval = 1000
-			resource_groups = "beyond"
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
 		}
-
+		
 		azure_app_gateway {
-			metrics_polling_interval = 1000
-			resource_groups = "beyond"
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
 		}
 		azure_app_service"{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-	}
-	azure_containers {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-	}
-
-	azure_cosmos_db {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-	}
-	azure_cost_management {
-		metrics_polling_interval = 1000
-		tag_keys = ""
-	}
-
-	azure_data_factory {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_event_hub {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_express_route {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_event_hub {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_firewalls {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_front_door {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_functions {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_key_vault {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_load_balancer {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_logic_apps {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_machine_learning {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_maria_db {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_mysql {
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_postgresql{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_power_bi_dedicated{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-	azure_power_bi_dedicated{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_redis_cache{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_service_bus{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_sql{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_storage{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_virtual_machine{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_virtual_networks{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_vms{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
-
-	azure_vpn_gateway{
-		metrics_polling_interval = 1000
-		resource_groups = "beyond"
-
-	}
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+		}
+		azure_containers {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+					
+		azure_cosmos_db {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_cost_management {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		
+		azure_data_factory {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		
+		azure_event_hub {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		
+		azure_express_route {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_event_hub {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_firewalls {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_front_door {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_functions {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_key_vault {
+				metrics_polling_interval = 1000
+				resource_groups = "beyond"
+				}
+		azure_load_balancer { 
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+				}
+		azure_logic_apps {
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		azure_machine_learning {
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		azure_maria_db {
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		
+		azure_mysql {
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		azure_postgresql{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		azure_power_bi_dedicated{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+				}
+		azure_power_bi_dedicated{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+				}
+		azure_redis_cache{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"	
+				}
+		
+		azure_service_bus{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"	
+				}
+		
+		azure_sql{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		
+		azure_storage{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		
+		azure_virtual_machine{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		
+		azure_virtual_networks{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		
+		azure_vms{
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
+		
+		azure_vpn_gateway{	
+				metrics_polling_interval = 1000	
+				resource_groups = "beyond"
+		
+				}
 	`, applicationID, clientSecretID, subscriptionID, tenantID, name)
 
 }
