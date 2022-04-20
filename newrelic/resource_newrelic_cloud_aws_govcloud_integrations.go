fcd39cc727c286e9b584180f56c10de6c5426427
K Sandeep Kumar <skumarreddy@newrelic.com>
2022-04-20 12:24:39 +0530

    chore: error fixes

--- newrelic/resource_newrelic_cloud_aws_govcloud_integrations.go
+++ newrelic/resource_newrelic_cloud_aws_govcloud_integrations.go
@@ -2,10 +2,11 @@ package newrelic
 
 import (
 	"context"
+	"strconv"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/newrelic/newrelic-client-go/pkg/cloud"
-	"strconv"
 )
 
 func resourceNewRelicAwsGovCloudIntegrations() *schema.Resource {
@@ -1021,7 +1022,7 @@ func expandAwsGovCloudIntegrationsAPIGatewayInput(b []interface{}, linkedAccount
 		}
 
 		if ft, ok := in["stage_prefixes"]; ok {
-			apiGatewayInput.StagePrefixes = ft.([]int)
+			apiGatewayInput.StagePrefixes = ft.([]string)
 
 		}
 		if tk, ok := in["tag_key"]; ok {
@@ -1752,7 +1753,7 @@ func expandAwsGovCloudIntegrationsSqsInput(b []interface{}, linkedAccountID int)
 			sqsInput.FetchTags = f.(bool)
 		}
 		if f, ok := in["queue_prefixes"]; ok {
-			sqsInput.QueuePrefixes = f.(string)
+			sqsInput.QueuePrefixes = f.([]string)
 		}
 		if tk, ok := in["tag_key"]; ok {
 			sqsInput.TagKey = tk.(string)
