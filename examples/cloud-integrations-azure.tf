4bb6dc7b839efd7b27a4045bed4c7d1a47158b22
K Sandeep Kumar <skumarreddy@newrelic.com>
2022-04-21 13:50:35 +0530

    chore: adding examples for azure

--- examples/cloud-integrations-azure.tf
+++ examples/cloud-integrations-azure.tf
@@ -1,229 +1,53 @@
+
 /*
 
     Complete example to enable Azure integration with New Relic
 
 */
 
-variable "NEW_RELIC_ACCOUNT_ID" {
-  type = string
-}
-
-variable "NEW_RELIC_CLOUDWATCH_ENDPOINT" {
-  type = string
-  # Depending on where your New Relic Account is located you need to change the default
-  default = "https://aws-api.newrelic.com/cloudwatch-metrics/v1" # US Datacenter
-  # default = "https://aws-api.eu01.nr-data.net/cloudwatch-metrics/v1" # EU Datacenter
-}
-
-data "aws_iam_policy_document" "newrelic_assume_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "AWS"
-      // This is the unique identifier for New Relic account on AWS, there is no need to change this
-      identifiers = [754728514883]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "sts:ExternalId"
-      values   = [var.NEW_RELIC_ACCOUNT_ID]
-    }
-  }
-}
-
-resource "aws_iam_role" "newrelic_aws_role" {
-  name               = "NewRelicInfrastructure-Integrations"
-  description        = "New Relic Cloud integration role"
-  assume_role_policy = data.aws_iam_policy_document.newrelic_assume_policy.json
-}
-
-resource "aws_iam_policy" "newrelic_aws_permissions" {
-  name        = "NewRelicCloudStreamReadPermissions"
-  description = ""
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "budgets:ViewBudget",
-        "cloudtrail:LookupEvents",
-        "config:BatchGetResourceConfig",
-        "config:ListDiscoveredResources",
-        "ec2:DescribeInternetGateways",
-        "ec2:DescribeVpcs",
-        "ec2:DescribeNatGateways",
-        "ec2:DescribeVpcEndpoints",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeNetworkAcls",
-        "ec2:DescribeVpcAttribute",
-        "ec2:DescribeRouteTables",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeVpcPeeringConnections",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DescribeVpnConnections",
-        "health:DescribeAffectedEntities",
-        "health:DescribeEventDetails",
-        "health:DescribeEvents",
-        "tag:GetResources",
-        "xray:BatchGet*",
-        "xray:Get*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "newrelic_aws_policy_attach" {
-  role       = aws_iam_role.newrelic_aws_role.name
-  policy_arn = aws_iam_policy.newrelic_aws_permissions.arn
-}
-
-resource "newrelic_cloud_aws_link_account" "newrelic_cloud_integration_push" {
-  arn = aws_iam_role.newrelic_aws_role.arn
-  metric_collection_mode = "PUSH"
-  name = "production-push"
-  depends_on = [aws_iam_role_policy_attachment.newrelic_aws_policy_attach]
-}
-
-resource "newrelic_api_access_key" "newrelic_aws_access_key" {
-  account_id  = var.NEW_RELIC_ACCOUNT_ID
-  key_type    = "INGEST"
-  ingest_type = "LICENSE"
-  name        = "Ingest License key"
-  notes       = "AWS Cloud Integrations Firehost Key"
-}
-
-resource "aws_iam_role" "firehose_newrelic_role" {
-  name = "firehose_newrelic_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "firehose.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "random_string" "s3-bucket-name" {
-  length = 8
-  special = false
-  upper = false
-}
-
-resource "aws_s3_bucket" "newrelic_aws_bucket" {
-  bucket = "newrelic-aws-bucket-${random_string.s3-bucket-name.id}"
-}
-
-resource "aws_s3_bucket_acl" "newrelic_aws_bucket_acl" {
-  bucket = aws_s3_bucket.newrelic_aws_bucket.id
-  acl    = "private"
-}
-
-resource "aws_kinesis_firehose_delivery_stream" "newrelic_firehost_stream" {
-  name        = "newrelic_firehost_stream"
-  destination = "http_endpoint"
-
-  s3_configuration {
-    role_arn           = aws_iam_role.firehose_newrelic_role.arn
-    bucket_arn         = aws_s3_bucket.newrelic_aws_bucket.arn
-    buffer_size        = 10
-    buffer_interval    = 400
-    compression_format = "GZIP"
-  }
-
-  http_endpoint_configuration {
-    url                = var.NEW_RELIC_CLOUDWATCH_ENDPOINT
-    name               = "New Relic"
-    access_key         = newrelic_api_access_key.newrelic_aws_access_key.key
-    buffering_size     = 1
-    buffering_interval = 60
-    role_arn           = aws_iam_role.firehose_newrelic_role.arn
-    s3_backup_mode     = "FailedDataOnly"
-
-    request_configuration {
-      content_encoding = "GZIP"
-    }
-  }
-}
-
-resource "aws_iam_role" "metric_stream_to_firehose" {
-  name = "metric_stream_to_firehose_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "streams.metrics.cloudwatch.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "metric_stream_to_firehose" {
-  name = "default"
-  role = aws_iam_role.metric_stream_to_firehose.id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "firehose:PutRecord",
-                "firehose:PutRecordBatch"
-            ],
-            "Resource": "${aws_kinesis_firehose_delivery_stream.newrelic_firehost_stream.arn}"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_cloudwatch_metric_stream" "newrelic_metric_stream" {
-  name          = "newrelic-metric-stream"
-  role_arn      = aws_iam_role.metric_stream_to_firehose.arn
-  firehose_arn  = aws_kinesis_firehose_delivery_stream.newrelic_firehost_stream.arn
-  output_format = "opentelemetry0.7"
-}
-
-resource "newrelic_cloud_aws_link_account" "newrelic_cloud_integration_pull" {
+resource "newrelic_cloud_azure_link_account" "newrelic_cloud_azure_integration_bar" {
   account_id = var.NEW_RELIC_ACCOUNT_ID
-  arn = aws_iam_role.newrelic_aws_role.arn
-  metric_collection_mode = "PULL"
-  name = "production-pull"
-  depends_on = [aws_iam_role_policy_attachment.newrelic_aws_policy_attach]
-}
+    application_id = "%[1]s"
+    client_secret = "%[2]s"
+    subscription_id = "%[3]s"
+    tenant_id = "%[4]s"
+    name  = "production-pull"
+  }
 
-resource "newrelic_cloud_aws_integrations" "foo" {
-  account_id = var.NEW_RELIC_ACCOUNT_ID
-  linked_account_id = newrelic_cloud_aws_link_account.newrelic_cloud_integration_pull.id
-  billing {}
-  cloudtrail {}
-  health {}
-  trusted_advisor {}
-  vpc {}
-  x_ray {}
+
+resource "newrelic_cloud_azure_integrations" "foo" {
+  account_id        = var.NEW_RELIC_ACCOUNT_ID
+  linked_account_id = newrelic_cloud_azure_link_account.newrelic_cloud_azure_integration_bar.id
+
+  api_management {}
+  app_gateway {}
+  app_service {}
+  containers {}
+  cosmos_db {}
+  cost_management {}
+  data_factory {}
+  event_hub {}
+  express_route {}
+  firewalls {}
+  front_door {}
+  functions {}
+  key_vault {}
+  load_balancer {}
+  logic_apps {}
+  machine_learning {}
+  maria_db {
+  mysql {}
+  postgresql {}
+  power_bi_dedicated {}
+  redis_cache {}
+  service_bus {}
+  service_fabric{}
+  sql {}
+  sql_managed{}
+  storage {}
+  virtual_machine {}
+  virtual_networks {}
+  vms {}
+  vpn_gateway {}
+
 }
