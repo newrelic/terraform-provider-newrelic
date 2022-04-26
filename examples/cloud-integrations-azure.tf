a1ab13664dfdc654e6d44e5e0eb96714c692645f
K Sandeep Kumar <skumarreddy@newrelic.com>
2022-04-26 20:24:10 +0530

    chore: adding examples

--- examples/cloud-integrations-azure.tf
+++ examples/cloud-integrations-azure.tf
@@ -5,6 +5,58 @@
 
 */
 
+terraform {
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+provider "newrelic" {
+
+  region = "US" # US or EU
+}
+
+data "azuread_client_config" "current" {}
+data "azurerm_subscription" "primary" {}
+
+resource "azuread_application" "example" {
+  display_name = "NewRelic-Integrations-201"
+  owners           = [data.azuread_client_config.current.object_id]
+  web {
+    redirect_uris = ["https://newrelic.com/"]
+  }
+}
+
+resource "time_rotating" "example" {
+  rotation_months = 6
+}
+
+resource "azuread_application_password" "example" {
+  application_object_id = azuread_application.example.object_id
+  rotate_when_changed = {
+    rotation = time_rotating.example.id
+  }
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = data.azuread_client_config.current.object_id
+}
+
 resource "newrelic_cloud_azure_link_account" "newrelic_cloud_azure_integration_bar" {
   account_id = var.NEW_RELIC_ACCOUNT_ID
     application_id = "%[1]s"
