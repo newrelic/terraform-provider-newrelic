8745159ab368e1ee1882c0943847cc27e1d94dc7
K Sandeep Kumar <skumarreddy@newrelic.com>
2022-05-26 12:42:10 +0530

    feat: implemanting the synthetics private locations

--- newrelic/resource_newrelic_synthetics_private_locations.go
+++ newrelic/resource_newrelic_synthetics_private_locations.go
@@ -2,10 +2,8 @@ package newrelic
 
 import (
 	"context"
-	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/newrelic/newrelic-client-go/pkg/entities"
 	"github.com/newrelic/newrelic-client-go/pkg/synthetics"
 )
 
@@ -73,11 +71,12 @@ func resourceNewRelicSyntheticsPrivateLocationCreate(ctx context.Context, d *sch
 	providerConfig := meta.(*ProviderConfig)
 	client := providerConfig.NewClient
 	accountID := selectAccountID(providerConfig, d)
-	pl := expandSyntheticsPrivateLocation(d)
-
 	var diags diag.Diagnostics
 
-	res, err := client.Synthetics.SyntheticsCreatePrivateLocationWithContext(ctx,accountID,"description","syn",true)
+	description := d.Get("description").(string)
+	name := d.Get("name").(string)
+	verifiedScriptExecution := d.Get("verifiedScriptExecution").(bool)
+	res, err := client.Synthetics.SyntheticsCreatePrivateLocationWithContext(ctx, accountID, description, name, verifiedScriptExecution)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -95,43 +94,69 @@ func resourceNewRelicSyntheticsPrivateLocationCreate(ctx context.Context, d *sch
 		return diags
 	}
 
-	d.SetId(res.Key)
-
-	return resourceNewRelicSyntheticsPrivateLocationRead(ctx, d, meta)
-}
-
-func resourceNewRelicSyntheticsPrivateLocationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	providerConfig := meta.(*ProviderConfig)
-	client := providerConfig.NewClient
-	queryString := fmt.Sprintf("domain = 'SYNTH' AND type = 'SECURE_CRED' AND name = %s", d.Id())
-
-	//entityResults, err := client.Entities.GetEntitySearchWithContext(ctx, entities.EntitySearchOptions{}, queryString, entities.EntitySearchQueryBuilder{}, []entities.EntitySearchSortCriteria{})
-	//if err != nil {
-	//
-	//}
-	//var entity *entities.EntityOutlineInterface
-	//for _, e := range entityResults.Results.Entities {
-	//	if e.GetName() == d.Id() {
-	//		entity = &e
-	//		break
-	//	}
-	//}
+	_ = d.Set("guid", res.GUID)
 
 	return nil
 }
 
-func expandSyntheticsPrivateLocation(d *schema.ResourceData) *synthetics.SecureCredential {
+func resourceNewRelicSyntheticsPrivateLocationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 
-	pl := synthetics.
-		Description:             d.Get("description").(string),
-		Name:                    d.Get(),
-		VerifiedScriptExecution: d.Get("boolean").(true),
-	}
-
-	return &pl
+	return nil
 }
 
-func flattenSyntheticsPrivateLocation(pl *synthetics.SyntheticsPrivateLocationMutationResult, d *schema.ResourceData) error {
+func resourceNewRelicSyntheticsPrivateLocationUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	providerConfig := meta.(*ProviderConfig)
+	client := providerConfig.NewClient
+	var diags diag.Diagnostics
+	description := d.Get("description").(string)
+	guid := synthetics.EntityGUID(d.Id())
+	verifiedScriptExecution := d.Get("verifiedScriptExecution").(bool)
+	res, err := client.Synthetics.SyntheticsUpdatePrivateLocation(description, guid, verifiedScriptExecution)
 
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if len(res.Errors) > 0 {
+		for _, err := range res.Errors {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  err.Description,
+			})
+		}
+	}
+
+	if len(diags) > 0 {
+		return diags
+	}
+
+	return nil
+}
+
+func resourceNewRelicSyntheticsPrivateLocationDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	providerConfig := meta.(*ProviderConfig)
+
+	client := providerConfig.NewClient
+	var diags diag.Diagnostics
+	guid := synthetics.EntityGUID(d.Id())
+	res, err := client.Synthetics.SyntheticsDeletePrivateLocationWithContext(ctx, guid)
+
+	if err != nil {
+		return diag.FromErr(err) //delete error return
+	}
+	if res != nil {
+		for _, err := range res.Errors {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  err.Description,
+			})
+		}
+	}
+
+	if len(diags) > 0 {
+		return diags
+	}
+
+	d.SetId("")
 	return nil
 }
