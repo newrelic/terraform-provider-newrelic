fddcc15aebdacf5b4b90ce6368b8336156ad5402
K Sandeep Kumar <skumarreddy@newrelic.com>
2022-05-30 11:54:30 +0530

    feat(synthetics): added private location resource

---
+++ newrelic/resource_newrelic_synthetics_private_location_test.go
@@ -0,0 +1,126 @@
+//go:build integration
+// +build integration
+
+package newrelic
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/newrelic/newrelic-client-go/pkg/entities"
+)
+
+func TestAccNewRelicSyntheticsPrivateLocation_Basic(t *testing.T) {
+	resourceName := "newrelic_synthetics_private_location.bar"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicSyntheticsPrivateLocationDestroy,
+		Steps: []resource.TestStep{
+			// Test: Create
+			{
+				Config: testAccNewRelicSyntheticsPrivateLocationConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsPrivateLocationExists(resourceName),
+				),
+			},
+			// Test: Update
+			{
+				Config: testAccNewRelicSyntheticsPrivateLocationConfigUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsPrivateLocationExists(resourceName),
+				),
+			},
+			// Test: Import
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"account_id", "description", "domain_id", "guid", "key", "location_id", "name", "verified_script_execution"},
+			},
+		},
+	})
+}
+
+func testAccCheckNewRelicSyntheticsPrivateLocationExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no synthetics private location is set")
+		}
+
+		client := testAccProvider.Meta().(*ProviderConfig).NewClient
+
+		queryString := fmt.Sprintf("domain = 'SYNTH' AND type = 'PRIVATE_LOCATION' AND name = '%s'", rs.Primary.ID)
+		time.Sleep(10 * time.Second)
+
+		found, err := client.Entities.GetEntitySearchByQuery(entities.EntitySearchOptions{}, queryString, []entities.EntitySearchSortCriteria{})
+		if err != nil {
+			return err
+		}
+
+		for _, e := range found.Results.Entities {
+			if !strings.EqualFold(string(e.GetGUID()), rs.Primary.ID) {
+				return fmt.Errorf("synthetics private location not found: %v - %v", rs.Primary.ID, found)
+			}
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckNewRelicSyntheticsPrivateLocationDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*ProviderConfig).NewClient
+	for _, r := range s.RootModule().Resources {
+		if r.Type != "newrelic_synthetics_private_location" {
+			continue
+		}
+
+		queryString := fmt.Sprintf("domain = 'SYNTH' AND type = 'PRIVATE_LOCATION' AND name = '%s'", r.Primary.ID)
+
+		time.Sleep(10 * time.Second)
+
+		found, err := client.Entities.GetEntitySearchByQuery(entities.EntitySearchOptions{}, queryString, []entities.EntitySearchSortCriteria{})
+		if err != nil {
+			return err
+		}
+
+		if found.Count != 0 {
+			return fmt.Errorf("synthetics private location still exists")
+		}
+
+	}
+	return nil
+}
+
+func testAccNewRelicSyntheticsPrivateLocationConfig(name string) string {
+	return fmt.Sprintf(`
+	resource "newrelic_synthetics_private_location" "bar" {
+	account_id	 = 2520528
+	description  = "Test Description"
+	name		 =	"%[1]s"
+	verified_script_execution = true
+}
+`, name)
+}
+
+func testAccNewRelicSyntheticsPrivateLocationConfigUpdated(name string) string {
+	return fmt.Sprintf(`
+	resource "newrelic_synthetics_private_location" "bar" {
+	account_id	 = 2520528
+	description  = "Test Description-Updated"
+	name		 =	"%[1]s-updated"
+	verified_script_execution = false
+}
+`, name)
+}
---
+++ website/docs/r/synthetics_private_location.html.markdown
@@ -0,0 +1,48 @@
+---
+layout: "newrelic"
+page_title: "New Relic: newrelic_synthetics_private_location"
+sidebar_current: "docs-newrelic-resource-synthetics-private-location"
+description: |-
+Create and manage Synthetics private location in New Relic.
+---
+
+# Resource: newrelic\_synthetics\_private\_location
+
+Use this resource to create and manage New Relic Synthetic private location.
+
+## Example Usage
+
+```hcl
+resource "newrelic_synthetics_private_location" "bar" {
+ accountId = "NewRelic account ID"
+ description = "The private location description"
+ name = "The name of the private location"
+ verifiedScriptExecution = "true or false"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `account_id` - (Optional) Determines the New Relic account where the secure credential will be created. Defaults to the account associated with the API key used.
+* `description` - The private location description.
+* `name` - (Required) The name of the private location.
+* `verifiedScriptExecution` - (Required) The private location requires a password to edit if value is true.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `domainId` - The private location globally unique identifier.
+* `guid` - The unique client identifier for the Synthetics private location in New Relic.
+* `locationId` - An alternate identifier based on name.
+* `key` - The private locations key.
+
+## Import
+
+A Synthetics private location can be imported using the `guid`
+
+```
+$ terraform import newrelic_synthetics_private_location.bar GUID
+```
