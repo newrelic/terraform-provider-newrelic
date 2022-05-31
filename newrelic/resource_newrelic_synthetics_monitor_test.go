d094f5b1de60e7051461702c4f7e202268d30317
shivabandari <sbandari@newrelic.com>
2022-05-31 15:19:39 +0530

    chore: added scripted monitors test cases

--- newrelic/resource_newrelic_synthetics_monitor_test.go
+++ newrelic/resource_newrelic_synthetics_monitor_test.go
@@ -533,3 +533,187 @@ func testAccNewRelicSyntheticsSimpleBrowserMonitorConfigUpdated(name string) str
 		}
 		`, name)
 }
+
+////////////////////////
+//scripted-api-monitor//
+////////////////////////
+
+func TestAccNewRelicSyntheticsScriptedAPIMonitor(t *testing.T) {
+	resourceName := "newrelic_synthetics_monitor.foo"
+	rName := acctest.RandString(5)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicSyntheticsMonitorDestroy,
+		Steps: []resource.TestStep{
+			//Test: Create
+			{
+				Config: testAccNewRelicSyntheticsScriptedAPIMonitorConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsMonitorExists(resourceName),
+				),
+			},
+			// Test: Update
+			{
+				Config: testAccNewRelicSyntheticsScriptedAPIMonitorConfigUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsMonitorExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func testAccNewRelicSyntheticsScriptedAPIMonitorConfig(name string) string {
+	fmt.Sprintf(`
+		resource "newrelic_synthetics_monitor" "bar" {
+		  locations = ["AP_SOUTH_1"]
+		  name      = "%[1]s"
+		  frequency = 5
+		  runtime_type="NODE_API"
+		  script_language="JAVASCRIPT"
+		  runtime_type_version="16.10"
+		  script=<<EOF
+		  var assert = require('assert');
+		  $http.post('http://httpbin.org/post',
+		  // Post data
+		  {
+		  json: {
+		  widgetType: 'gear',
+		  widgetCount: 10
+		  }
+		  },
+		  // Callback
+		  function (err, response, body) {
+		  assert.equal(response.statusCode, 200, 'Expected a 200 OK response');
+		  console.log('Response:', body.json);
+		  assert.equal(body.json.widgetType, 'gear', 'Expected a gear widget type');
+		  assert.equal(body.json.widgetCount, 10, 'Expected 10 widgets');
+		  }
+		  );
+		  EOF
+		  status    = "ENABLED"
+		  type      = "SCRIPT_API"
+		  tags{
+			key="Name"
+			values=["SimpleMonitor"]
+		  }
+		}
+		`, name)
+}
+
+func testAccNewRelicSyntheticsScriptedAPIMonitorConfigUpdated(name string) string {
+	return fmt.Sprintf(`
+		resource "newrelic_synthetics_monitor" "bar" {
+		  locations = ["AP_SOUTH_1","AP_EAST_1"]
+		  name      = "%[1]s-updated"
+		  frequency = 10
+		  runtime_type="NODE_API"
+		  script_language="JAVASCRIPT"
+		  runtime_type_version="16.10"
+		  script=<<EOF
+		  var assert = require('assert');
+		  $http.post('http://httpbin.org/post',
+		  // Post data
+		  {
+		  json: {
+		  widgetType: 'gear',
+		  widgetCount: 10
+		  }
+		  },
+		  // Callback
+		  function (err, response, body) {
+		  assert.equal(response.statusCode, 200, 'Expected a 200 OK response');
+		  console.log('Response:', body.json);
+		  assert.equal(body.json.widgetType, 'gear', 'Expected a gear widget type');
+		  assert.equal(body.json.widgetCount, 10, 'Expected 10 widgets');
+		  }
+		  );
+		  EOF
+		  status    = "DISABLED"
+		  type      = "SCRIPT_API"
+		  tags{
+			key="Name"
+			values=["SimpleMonitor","hello"]
+		  }
+		}
+		`, name)
+}
+
+////////////////////////////
+//Scripted-Browser-monitor//
+////////////////////////////
+
+func TestAccNewRelicSyntheticsScriptedBrowserMonitor(t *testing.T) {
+	resourceName := "newrelic_synthetics_monitor.foo"
+	rName := acctest.RandString(5)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicSyntheticsMonitorDestroy,
+		Steps: []resource.TestStep{
+			//Test: Create
+			{
+				Config: testAccNewRelicSyntheticsScriptedBrowserMonitorConfig(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsMonitorExists(resourceName),
+				),
+			},
+			// Test: Update
+			{
+				Config: testAccNewRelicSyntheticsScriptedBrowserMonitorConfigUpdated(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicSyntheticsMonitorExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func testAccNewRelicSyntheticsScriptedBrowserMonitorConfig(name string) string {
+	return fmt.Sprintf(`
+		resource "newrelic_synthetics_monitor" "bar" {
+		  enable_screenshot_on_failure_and_script=true
+		  locations = ["AP_SOUTH_1"]
+		  name      = "%[1]s"
+		  frequency = 10
+		  runtime_type_version="100"
+		  runtime_type="CHROME_BROWSER"
+		  script_language="JAVASCRIPT"
+		  status    = "ENABLED"
+		  type      = "SCRIPT_BROWSER"
+		  script=<<EOF
+		  var assert = require('assert');
+		  $browser.get('https://one.newrelic.com')
+		  EOF
+		  tags{
+			key="Name"
+			values=["scriptedMonitor"]
+		  }
+		}
+		`, name)
+}
+
+func testAccNewRelicSyntheticsScriptedBrowserMonitorConfigUpdated(name string) string {
+	return fmt.Sprintf(`
+		resource "newrelic_synthetics_monitor" "bar" {
+		  enable_screenshot_on_failure_and_script=false
+		  locations = ["AP_SOUTH_1","AP_EAST_1"]
+		  name      = "%[1]s_updated"
+		  frequency = 10
+		  runtime_type_version="100"
+		  runtime_type="CHROME_BROWSER"
+		  script_language="JAVASCRIPT"
+		  status    = "DISABLED"
+		  type      = "SCRIPT_BROWSER"
+		  script=<<EOF
+		  var assert = require('assert');
+		  $browser.get('https://one.newrelic.com')
+		  EOF
+		  tags{
+			key="Name"
+			values=["scriptedMonitor","hello"]
+		  }
+		}
+		`, name)
+}
