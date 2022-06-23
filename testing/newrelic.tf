8d3a1059f3affc2feee216998b0736dc67af8469
shivabandari <sbandari@newrelic.com>
2022-06-23 19:07:22 +0530

    chore: added expansion for reading the schema values

--- testing/newrelic.tf
+++ testing/newrelic.tf
@@ -11,20 +11,26 @@ provider "newrelic" {
   region = "US" # US or EU
 }
 
-resource "newrelic_synthetics_script_monitor" "bar" {
-  enable_screenshot_on_failure_and_script	=	true
-  locations_public	=	["AP_SOUTH_1"]
-  name	=	"Script_name"
-  period	=	"EVERY_15_MINUTES"
-  runtime_type_version	=	"100"
-  runtime_type	=	"CHROME_BROWSER"
-  script_language	=	"JAVASCRIPT"
-  status	=	"ENABLED"
-  type	=	"SCRIPT_BROWSER"
-  script	=	"$browser.get('https://one.newrelic.com')"
-  tag {
-    key	= "Name"
-    values	= ["scriptedMonitor","simpler"]
+resource "newrelic_synthetics_monitor" "foo" {
+  custom_header{
+    name	=	"name"
+    value	=	"simpleMonitorUpdated"
   }
+  treat_redirect_as_failure	=	false
+  validation_string	=	"success"
+  bypass_head_request	=	false
+  verify_ssl	=	false
+  //false to true --w
+  //true to false --nw
+  location_public	=	["AP_SOUTH_1","AP_EAST_1"]
+  name	=	"%s-updated"
+  period	=	"EVERY_15_MINUTES"
+  status	=	"ENABLED"
+  type	=	"SIMPLE"
+#  tag {
+#    key	=	"Name"
+#    values	=	[ "myMonitor","simple_monitor"]
+#  }
+  uri	=	"https://one.newrelic.com"
 }
 
