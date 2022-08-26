a490be19bf431db0fed013b1ea2f8aa39247abe9
Samuel Vandamme <samuel@sava.be>
2022-08-26 13:31:35 +0200

    chore(newrelic_alert_policy): removed computed from account_id

--- newrelic/resource_newrelic_alert_policy.go
+++ newrelic/resource_newrelic_alert_policy.go
@@ -34,7 +34,7 @@ func resourceNewRelicAlertPolicy() *schema.Resource {
 			"account_id": {
 				Type:        schema.TypeInt,
 				Optional:    true,
-				Computed:    true,
+				ForceNew:    true,
 				Description: "The New Relic account ID to operate on.",
 			},
 			"incident_preference": {
