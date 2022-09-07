38d5813233e4d4cd9ff00ec8e340617dd1247da3
NSSPKrishna <pniladhuri@newrelic.com>
2022-09-07 18:18:56 +0530

    chore(docs): private locations datasource additional example

--- testing/newrelic.tf
+++ testing/newrelic.tf
@@ -9,11 +9,3 @@ terraform {
 provider "newrelic" {
   region = "US" # US or EU
 }
-
-data "newrelic_synthetics_private_location" "example" {
-  name = "My private location"
-}
-resource "newrelic_synthetics_monitor" "foo" {
-  // Reference the private location data source in the monitor resource
-  location_private { guid = data.newrelic_synthetics_private_location.example.id }
-}
