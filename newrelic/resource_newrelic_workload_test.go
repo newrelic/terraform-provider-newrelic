54b0a53ae4f069ac7ba3d5a4a2857ed98095a5dc
NSSPKrishna <pniladhuri@newrelic.com>
2022-09-26 15:21:56 +0530

    chore(workloads): Added tests

--- newrelic/resource_newrelic_workload_test.go
+++ newrelic/resource_newrelic_workload_test.go
@@ -36,12 +36,12 @@ func TestAccNewRelicWorkload_Basic(t *testing.T) {
 				),
 			},
 			// Test: Import
-			//{
-			//	ResourceName:            resourceName,
-			//	ImportState:             true,
-			//	ImportStateVerify:       true,
-			//	ImportStateVerifyIgnore: []string{"entity_search_query", "composite_entity_search_query"},
-			//},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"entity_search_query", "composite_entity_search_query"},
+			},
 		},
 	})
 }
@@ -122,6 +122,159 @@ func TestAccNewRelicWorkload_EntityScopeAccountsOnly(t *testing.T) {
 	})
 }
 
+////TODO changes
+func TestAccNewRelicWorkload_BasicOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadBasicConfigOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_StaticOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadConfigSaticOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_AutomaticOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadAutomaticConfigOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_AutomaticEnabledOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadAutomaticConfig_EnabledOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_AutomaticRemainingEntitiesOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadAutomaticConfig_RemainingEntitiesOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_AutomaticRuleOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadAutomaticConfig_RuleOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_AutomaticRulesOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadAutomaticConfig_RulesOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicWorkload_AutomaticRuleRollupOnly(t *testing.T) {
+	resourceName := "newrelic_workload.foo"
+	rName := acctest.RandString(5)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNewRelicWorkloadDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicWorkloadAutomaticConfig_RuleRollupOnly(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicWorkloadExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckNewRelicWorkloadExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
@@ -193,37 +346,37 @@ resource "newrelic_workload" "foo" {
 
 	scope_account_ids =  [%[1]d]
 
-  description = "Something"
+  	description = "something"
 
-  status_config_automatic {
-    enabled = true
-remaining_entities_rule{
-	remaining_entities_rule_rollup {
-      strategy = "BEST_STATUS_WINS"
-      threshold_type = "FIXED"
-      threshold_value = 100
-      group_by = "ENTITY_TYPE"
+	status_config_automatic {
+		enabled = true
+		remaining_entities_rule{
+			remaining_entities_rule_rollup {
+			  strategy = "BEST_STATUS_WINS"
+			  threshold_type = "FIXED"
+			  threshold_value = 100
+			  group_by = "ENTITY_TYPE"
+			}
+		}
+		rules{
+		 entity_guids = [data.newrelic_entity.app.guid]
+		 nrql_query{
+		   query = "name like 'ok'"
+		 }
+		rollup{
+			strategy = "BEST_STATUS_WINS"
+			threshold_type = "FIXED"
+			threshold_value = 100
+			}
+		}
 	}
-}
-   rules{
-     //entity_guids = ["MjUyMDUyOHxTWU5USHxNT05JVE9SfGQyN2ExNmFhLWRjZmUtNDBlZi05YjgxLWFjZDI2ZTc4MmJkZg"]
-     nrql_query{
-       query = "name like 'ok'"
-     }
-      rollup{
-        strategy = "BEST_STATUS_WINS"
-        threshold_type = "FIXED"
-        threshold_value = 100
-      }
-    }
-  }
-
-  status_config_static {
-    description = "test"
-    enabled = true
-    status = "OPERATIONAL"
-    summary = "egetgykwesgksegkerh"
-  }
+	
+	status_config_static {
+	description = "test"
+	enabled = true
+	status = "OPERATIONAL"
+	summary = "egetgykwesgksegkerh"
+	}
 }
 
 `, testAccountID, name, testAccExpectedApplicationName)
@@ -306,6 +459,216 @@ resource "newrelic_workload" "foo" {
 	account_id = %[1]d
 
 	scope_account_ids =  [%[1]d]
+}
+`, testAccountID, name)
+}
+
+func testAccNewRelicWorkloadBasicConfigOnly(name string) string {
+	return fmt.Sprintf(`
+
+data "newrelic_entity" "app" {
+	name = "%[3]s"
+	domain = "APM"
+	type = "APPLICATION"
+}
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+	entity_guids = [data.newrelic_entity.app.guid]
+
+	entity_search_query {
+		query = "name like 'App'"
+	}
+
+	scope_account_ids =  [%[1]d]
+
+ 	description = "something"
+}
+
+`, testAccountID, name, testAccExpectedApplicationName)
+}
+
+func testAccNewRelicWorkloadConfigSaticOnly(name string) string {
+	return fmt.Sprintf(`
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+	status_config_static {
+		description = "test"
+		enabled = true
+		status = "OPERATIONAL"
+		summary = "egetgykwesgksegkerh"
+	  }
+}
+`, testAccountID, name)
+}
+
+func testAccNewRelicWorkloadAutomaticConfigOnly(name string) string {
+	return fmt.Sprintf(`
+
+data "newrelic_entity" "app" {
+	name = "%[3]s"
+	domain = "APM"
+	type = "APPLICATION"
+}
+
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+ 	description = "something"
+	status_config_automatic {
+		enabled = true
+		remaining_entities_rule{
+			remaining_entities_rule_rollup {
+			  strategy = "BEST_STATUS_WINS"
+			  threshold_type = "FIXED"
+			  threshold_value = 100
+			  group_by = "ENTITY_TYPE"
+			}
+		}
+		rules{
+		 entity_guids = [data.newrelic_entity.app.guid]
+		 nrql_query{
+		   query = "name like 'ok'"
+		 }
+		rollup{
+			strategy = "BEST_STATUS_WINS"
+			threshold_type = "FIXED"
+			threshold_value = 100
+			}
+		}
+	}
+}
+`, testAccountID, name, testAccExpectedApplicationName)
+}
+
+func testAccNewRelicWorkloadAutomaticConfig_EnabledOnly(name string) string {
+	return fmt.Sprintf(`
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+	status_config_automatic {
+		enabled = true
+	}
+}
+`, testAccountID, name)
+}
+
+func testAccNewRelicWorkloadAutomaticConfig_RemainingEntitiesOnly(name string) string {
+	return fmt.Sprintf(`
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+ 	description = "something"
+	status_config_automatic {
+		enabled = true
+		remaining_entities_rule{
+			remaining_entities_rule_rollup {
+			  strategy = "BEST_STATUS_WINS"
+			  threshold_type = "FIXED"
+			  threshold_value = 100
+			  group_by = "ENTITY_TYPE"
+			}
+		}
+	}
+}
+`, testAccountID, name)
+}
+
+func testAccNewRelicWorkloadAutomaticConfig_RuleOnly(name string) string {
+	return fmt.Sprintf(`
+
+data "newrelic_entity" "app" {
+	name = "%[3]s"
+	domain = "APM"
+	type = "APPLICATION"
+}
+
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+ 	description = "something"
+	status_config_automatic {
+		enabled = true
+		rules{
+		 entity_guids = [data.newrelic_entity.app.guid]
+		 nrql_query{
+		   query = "name like 'ok'"
+		 }
+			rollup{
+				strategy = "BEST_STATUS_WINS"
+				threshold_type = "FIXED"
+				threshold_value = 100
+			}
+		}
+	}
+}
+`, testAccountID, name, testAccExpectedApplicationName)
+}
+
+func testAccNewRelicWorkloadAutomaticConfig_RulesOnly(name string) string {
+	return fmt.Sprintf(`
+data "newrelic_entity" "app" {
+	name = "%[3]s"
+	domain = "APM"
+	type = "APPLICATION"
+}
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+	status_config_automatic {
+		enabled = true
+		rules{
+		 entity_guids = [data.newrelic_entity.app.guid]
+		 nrql_query{
+		   query = "name like 'ok'"
+		 }
+			rollup{
+				strategy = "BEST_STATUS_WINS"
+				threshold_type = "FIXED"
+				threshold_value = 100
+			}
+		}
+		rules{
+		 entity_guids = [data.newrelic_entity.app.guid]
+		 nrql_query{
+		   query = "name like 'ok'"
+		 }
+			rollup{
+				strategy = "BEST_STATUS_WINS"
+				threshold_type = "FIXED"
+				threshold_value = 100
+			}
+		}
+	}
+}
+`, testAccountID, name, testAccExpectedApplicationName)
+}
+
+func testAccNewRelicWorkloadAutomaticConfig_RuleRollupOnly(name string) string {
+	return fmt.Sprintf(`
+
+resource "newrelic_workload" "foo" {
+	name = "%[2]s"
+	account_id = %[1]d
+
+	status_config_automatic {
+		enabled = true
+		rules{
+			rollup{
+				strategy = "BEST_STATUS_WINS"
+				threshold_type = "FIXED"
+				threshold_value = 100
+			}
+		}
+	}
 }
 `, testAccountID, name)
 }
