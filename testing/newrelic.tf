c669b712fdbeea7b2b0a101779eea52dc391574c
Maria Bazhlekova <mbazhlek@gmail.com>
2022-10-18 17:09:00 -0500

    chore: remove test data

--- testing/newrelic.tf
+++ testing/newrelic.tf
@@ -9,11 +9,3 @@ terraform {
 provider "newrelic" {
   region = "US" # US or EU
 }
-
-resource "newrelic_workload" "foo" {
-  name = "tf-test-workload-mbazhlek"
-
-  entity_search_query {
-    query = "name like 'App'"
-  }
-}
