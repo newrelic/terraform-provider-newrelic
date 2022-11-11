0e8da14ab7cee6e78eacb32c67fdeb9cdfcb8b81
NSSPKrishna <pniladhuri@newrelic.com>
2022-11-11 17:18:34 +0530

    chore(deps): updated client go version

--- newrelic/resource_newrelic_obfuscation_expression.go
+++ newrelic/resource_newrelic_obfuscation_expression.go
@@ -6,7 +6,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/newrelic/newrelic-client-go/v2/newrelic"
-	_ "github.com/newrelic/newrelic-client-go/v2/newrelic"
 	"github.com/newrelic/newrelic-client-go/v2/pkg/logconfigurations"
 	"log"
 )
