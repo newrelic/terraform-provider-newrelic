0d7861616c33f746b9194b893688ffe38aa134bc
NSSPKrishna <pniladhuri@newrelic.com>
2023-03-03 16:28:05 +0530

    chore(data_partition): code cleanup

--- newrelic/resource_newrelic_data_partition_test.go
+++ newrelic/resource_newrelic_data_partition_test.go
@@ -96,7 +96,7 @@ func TestAccNewRelicDataPartitionRule_NameUpdate(t *testing.T) {
 }
 
 // Checking the creation of new resource on update in name
-func TestAccNewRelicDataPartitionRule_MatchingCriteriaUpdate(t *testing.T) {
+func TestAccNewRelicDataPartitionRule_NRQLUpdate(t *testing.T) {
 	resourceName := "newrelic_data_partition_rule.foo"
 	rName := acctest.RandString(7)
 	resource.ParallelTest(t, resource.TestCase{
@@ -112,7 +112,7 @@ func TestAccNewRelicDataPartitionRule_MatchingCriteriaUpdate(t *testing.T) {
 			},
 			//update
 			{
-				Config: testAccNewRelicDataPartitionRuleUpdate_MatchingCriteria(rName),
+				Config: testAccNewRelicDataPartitionRuleUpdate_NRQL(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckNewRelicDataPartitionRuleExists(resourceName)),
 			},
@@ -200,9 +200,6 @@ resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s"
 	enabled = true
-    attribute_name = "hostname"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s"
@@ -216,9 +213,6 @@ resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s_update"
 	enabled = true
-    attribute_name = "hostname_update"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='linux_messages'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s"
@@ -232,9 +226,6 @@ resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s"
 	enabled = true
-    attribute_name = "hostname"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s"
@@ -244,9 +235,6 @@ resource "newrelic_data_partition_rule" "bar"{
 	account_id = %[1]d
 	description = "%[3]s"
 	enabled = true
-    attribute_name = "hostname"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s"
@@ -260,9 +248,6 @@ resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s"
 	enabled = true
-    attribute_name = "hostname"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Test_%[2]s"
@@ -276,9 +261,6 @@ resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s_update"
 	enabled = false
-    attribute_name = "hostname_update"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s"
@@ -292,9 +274,6 @@ resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s_update"
 	enabled = true
-    attribute_name = "hostname_update"
-    matching_expression = "localhost"
-    matching_method = "EQUALS"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s_update"
@@ -302,15 +281,12 @@ resource "newrelic_data_partition_rule" "foo"{
 `, testAccountID, name, testAccExpectedApplicationName)
 }
 
-func testAccNewRelicDataPartitionRuleUpdate_MatchingCriteria(name string) string {
+func testAccNewRelicDataPartitionRuleUpdate_NRQL(name string) string {
 	return fmt.Sprintf(`
 resource "newrelic_data_partition_rule" "foo"{
 	account_id = %[1]d
 	description = "%[3]s_update"
 	enabled = true
-    attribute_name = "hostname_update"
-    matching_expression = "localhost"
-    matching_method = "LIKE"
 	nrql = "logtype='node'"
     retention_policy = "SECONDARY"
     target_data_partition = "Log_Test_%[2]s_update"
