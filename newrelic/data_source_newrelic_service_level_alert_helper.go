8e73e9ca8b0d4da6e855454cc267ec9a4a31a026
nventosa <ventosa@gmail.com>
2023-03-22 16:12:27 +0100

    feat: fix linting errors

--- newrelic/data_source_newrelic_service_level_alert_helper.go
+++ newrelic/data_source_newrelic_service_level_alert_helper.go
@@ -11,69 +11,69 @@ import (
 )
 
 const (
-	ALERT_TYPE_FIELD              = "alert_type"
-	SLI_GUID_FIELD                = "sli_guid"
-	SLO_TARGET_FIELD              = "slo_target"
-	SLO_PERIOD_FIELD              = "slo_period"
-	CUSTOM_CONSUMPTION_FIELD      = "custom_tolerated_budget_consumption"
-	CUSTOM_EVALUTION_PERIOD_FIELD = "custom_evaluation_period"
-	CONSUMPTION_FIELD             = "tolerated_budget_consumption"
-	EVALUATION_PERIOD_FIELD       = "evaluation_period"
-	THRESHOLD_FIELD               = "threshold"
-	NRQL_FIELD                    = "nrql"
+	AlertTypeField              = "alert_type"
+	SliGUIDField                = "sli_guid"
+	SloTargetField              = "slo_target"
+	SloPeriodField              = "slo_period"
+	CustomConsumptionField      = "custom_tolerated_budget_consumption"
+	CustomEvaluationPeriodField = "custom_evaluation_period"
+	ConsumptionField            = "tolerated_budget_consumption"
+	EvaluationPeriodField       = "evaluation_period"
+	ThresholdField              = "threshold"
+	NRQLField                   = "nrql"
 
-	CUSTOM                        = "custom"
-	FAST_BURN                     = "fast_burn"
-	FAST_BURN_PERIOD              = 60
-	FAST_BURN_CONSUMPTION         = 2
+	Custom              = "custom"
+	FastBurn            = "fast_burn"
+	FastBurnPeriod      = 60
+	FastBurnConsumption = 2
 )
 
 func dataSourceNewRelicServiceLevelAlertHelper() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceNewRelicServiceLevelAlertHelperRead,
 		Schema: map[string]*schema.Schema{
-			ALERT_TYPE_FIELD: {
+			AlertTypeField: {
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: validation.StringInSlice([]string{CUSTOM, FAST_BURN}, true),
+				ValidateFunc: validation.StringInSlice([]string{Custom, FastBurn}, true),
 			},
-			SLI_GUID_FIELD: {
+			SliGUIDField: {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			SLO_TARGET_FIELD: {
+			SloTargetField: {
 				Type:         schema.TypeFloat,
 				Required:     true,
 				ValidateFunc: validation.FloatBetween(0, 100),
 			},
-			SLO_PERIOD_FIELD: {
+			SloPeriodField: {
 				Type:         schema.TypeInt,
 				Required:     true,
 				ValidateFunc: validation.IntInSlice([]int{1, 7, 28}),
 			},
-			CUSTOM_CONSUMPTION_FIELD: {
+			CustomConsumptionField: {
 				Type:         schema.TypeFloat,
 				Optional:     true,
 				ValidateFunc: validation.FloatBetween(0, 100),
 			},
-			CUSTOM_EVALUTION_PERIOD_FIELD: {
+			CustomEvaluationPeriodField: {
 				Type:         schema.TypeInt,
 				Optional:     true,
 				ValidateFunc: validation.IntAtLeast(1),
 			},
-			CONSUMPTION_FIELD: {
+			ConsumptionField: {
 				Type:     schema.TypeFloat,
 				Computed: true,
 			},
-			EVALUATION_PERIOD_FIELD: {
+			EvaluationPeriodField: {
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
-			THRESHOLD_FIELD: {
+			ThresholdField: {
 				Type:     schema.TypeFloat,
 				Computed: true,
 			},
-			NRQL_FIELD: {
+			NRQLField: {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -83,67 +83,67 @@ func dataSourceNewRelicServiceLevelAlertHelper() *schema.Resource {
 
 func dataSourceNewRelicServiceLevelAlertHelperRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 
-	var sliGuid = d.Get(SLI_GUID_FIELD).(string)
+	var sliGUID = d.Get(SliGUIDField).(string)
 	rnd := strconv.Itoa(rand.Int())
-	d.SetId(sliGuid + rnd)
+	d.SetId(sliGUID + rnd)
 
-	var sloPeriod = d.Get(SLO_PERIOD_FIELD).(int)
-	var sloTarget = d.Get(SLO_TARGET_FIELD).(float64)
-	var alertType = d.Get(ALERT_TYPE_FIELD).(string)
+	var sloPeriod = d.Get(SloPeriodField).(int)
+	var sloTarget = d.Get(SloTargetField).(float64)
+	var alertType = d.Get(AlertTypeField).(string)
 
-	_, tOk := d.GetOk(CUSTOM_CONSUMPTION_FIELD)
-	_, eOk := d.GetOk(CUSTOM_EVALUTION_PERIOD_FIELD)
+	_, tOk := d.GetOk(CustomConsumptionField)
+	_, eOk := d.GetOk(CustomEvaluationPeriodField)
 
 	var err error
 
 	switch alertType {
-	case FAST_BURN:
+	case FastBurn:
 		if tOk || eOk {
-			return diag.Errorf("For " + FAST_BURN + " alert type do not fill '" + CUSTOM_EVALUTION_PERIOD_FIELD + "' or '" + CUSTOM_CONSUMPTION_FIELD + "'.")
+			return diag.Errorf("For " + FastBurn + " alert type do not fill '" + CustomEvaluationPeriodField + "' or '" + CustomConsumptionField + "'.")
 		}
 
-		threshold := calculateThreshold(sloTarget, FAST_BURN_CONSUMPTION, sloPeriod, FAST_BURN_PERIOD)
-		err = d.Set(THRESHOLD_FIELD, threshold)
+		threshold := calculateThreshold(sloTarget, FastBurnConsumption, sloPeriod, FastBurnPeriod)
+		err = d.Set(ThresholdField, threshold)
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
-		err = d.Set(EVALUATION_PERIOD_FIELD, FAST_BURN_PERIOD)
+		err = d.Set(EvaluationPeriodField, FastBurnPeriod)
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
-		err = d.Set(CONSUMPTION_FIELD, FAST_BURN_CONSUMPTION)
+		err = d.Set(ConsumptionField, FastBurnConsumption)
 		if err != nil {
 			return diag.FromErr(err)
 		}
-	case CUSTOM:
+	case Custom:
 		if !tOk || !eOk {
-			return diag.Errorf("For " + CUSTOM + " alert type the fields '" + CUSTOM_EVALUTION_PERIOD_FIELD + "' and '" + CUSTOM_CONSUMPTION_FIELD + "' are mandatory.")
+			return diag.Errorf("For " + Custom + " alert type the fields '" + CustomEvaluationPeriodField + "' and '" + CustomConsumptionField + "' are mandatory.")
 		}
 
-		toleratedBudgetConsumption := d.Get(CUSTOM_CONSUMPTION_FIELD).(float64)
-		evaluationPeriod := d.Get(CUSTOM_EVALUTION_PERIOD_FIELD).(int)
+		toleratedBudgetConsumption := d.Get(CustomConsumptionField).(float64)
+		evaluationPeriod := d.Get(CustomEvaluationPeriodField).(int)
 		var threshold = calculateThreshold(sloTarget, toleratedBudgetConsumption, sloPeriod, evaluationPeriod)
 
-		err = d.Set(THRESHOLD_FIELD, threshold)
+		err = d.Set(ThresholdField, threshold)
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
-		err = d.Set(EVALUATION_PERIOD_FIELD, evaluationPeriod)
+		err = d.Set(EvaluationPeriodField, evaluationPeriod)
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
-		err = d.Set(CONSUMPTION_FIELD, toleratedBudgetConsumption)
+		err = d.Set(ConsumptionField, toleratedBudgetConsumption)
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
 	}
 
-	err = d.Set(NRQL_FIELD, "FROM Metric SELECT 100 - clamp_max(sum(newrelic.sli.good) / sum(newrelic.sli.valid) * 100, 100) as 'SLO compliance'  WHERE sli.guid = '"+sliGuid+"'")
+	err = d.Set(NRQLField, "FROM Metric SELECT 100 - clamp_max(sum(newrelic.sli.good) / sum(newrelic.sli.valid) * 100, 100) as 'SLO compliance'  WHERE sli.guid = '"+sliGUID+"'")
 	if err != nil {
 		return diag.FromErr(err)
 	}
