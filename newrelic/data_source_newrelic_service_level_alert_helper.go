a068126feab1c5059dbd28769ca5897283f67323
nventosa <ventosa@gmail.com>
2023-03-22 15:55:32 +0100

    feat: implementation and tests for data source service_level_alert_helper

--- newrelic/data_source_newrelic_service_level_alert_helper.go
+++ newrelic/data_source_newrelic_service_level_alert_helper.go
@@ -2,108 +2,155 @@ package newrelic
 
 import (
 	"context"
+	"math/rand"
+	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-func dataSourceNewRelicServiceLevelAlertHelper() *schema.Resource { 
-    return &schema.Resource{
+const (
+	ALERT_TYPE_FIELD              = "alert_type"
+	SLI_GUID_FIELD                = "sli_guid"
+	SLO_TARGET_FIELD              = "slo_target"
+	SLO_PERIOD_FIELD              = "slo_period"
+	CUSTOM_CONSUMPTION_FIELD      = "custom_tolerated_budget_consumption"
+	CUSTOM_EVALUTION_PERIOD_FIELD = "custom_evaluation_period"
+	CONSUMPTION_FIELD             = "tolerated_budget_consumption"
+	EVALUATION_PERIOD_FIELD       = "evaluation_period"
+	THRESHOLD_FIELD               = "threshold"
+	NRQL_FIELD                    = "nrql"
+
+	CUSTOM                        = "custom"
+	FAST_BURN                     = "fast_burn"
+	FAST_BURN_PERIOD              = 60
+	FAST_BURN_CONSUMPTION         = 2
+)
+
+func dataSourceNewRelicServiceLevelAlertHelper() *schema.Resource {
+	return &schema.Resource{
 		ReadContext: dataSourceNewRelicServiceLevelAlertHelperRead,
 		Schema: map[string]*schema.Schema{
-			"sli_guid": {
-				Type:        schema.TypeString,
-				Required:    true,
+			ALERT_TYPE_FIELD: {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice([]string{CUSTOM, FAST_BURN}, true),
 			},
-			"slo_target": {
-				Type:        schema.TypeFloat,
-				Required:    true,
-                ValidateFunc: validation.FloatBetween(0, 100),
+			SLI_GUID_FIELD: {
+				Type:     schema.TypeString,
+				Required: true,
 			},
-			"slo_period": {
-				Type:        schema.TypeInt,
-				Required:    true,
-                ValidateFunc: validation.IntInSlice([]int{1,7,28}),
+			SLO_TARGET_FIELD: {
+				Type:         schema.TypeFloat,
+				Required:     true,
+				ValidateFunc: validation.FloatBetween(0, 100),
 			},
-			"custom_tolerated_budget_consumption": {
-				Type:        schema.TypeFloat,
-                Optional:    true,
-                ValidateFunc: validation.FloatBetween(0, 100),
+			SLO_PERIOD_FIELD: {
+				Type:         schema.TypeInt,
+				Required:     true,
+				ValidateFunc: validation.IntInSlice([]int{1, 7, 28}),
 			},
-            "custom_evaluation_period": {
-                Type: schema.TypeInt,
-                Optional:    true,
-                ValidateFunc: validation.IntAtLeast(1),
-            },
-            "custom_threshold": {
-                Type: schema.TypeFloat,
-                Computed: true,
-            },
-            "fast_burn_threshold": {
-                Type: schema.TypeFloat,
-                Computed: true,
-            },
-            "fast_burn_evaluation_period": {
-                Type: schema.TypeInt,
-                Computed: true,
-            },
-            "slow_burn_threshold": {
-                Type: schema.TypeFloat,
-                Computed: true,
-            },
-            "slow_burn_evaluation_period": {
-                Type: schema.TypeInt,
-                Computed: true,
-            },
+			CUSTOM_CONSUMPTION_FIELD: {
+				Type:         schema.TypeFloat,
+				Optional:     true,
+				ValidateFunc: validation.FloatBetween(0, 100),
+			},
+			CUSTOM_EVALUTION_PERIOD_FIELD: {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ValidateFunc: validation.IntAtLeast(1),
+			},
+			CONSUMPTION_FIELD: {
+				Type:     schema.TypeFloat,
+				Computed: true,
+			},
+			EVALUATION_PERIOD_FIELD: {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			THRESHOLD_FIELD: {
+				Type:     schema.TypeFloat,
+				Computed: true,
+			},
+			NRQL_FIELD: {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
 
 func dataSourceNewRelicServiceLevelAlertHelperRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-    d.SetId(d.Get("sli_guid").(string))
 
-	var sloPeriod = d.Get("slo_period").(int)
-	var sloTarget = d.Get("slo_target").(float64)
-    _, tOk := d.GetOk("custom_tolerated_budget_consumption")
-    _, eOk := d.GetOk("custom_evaluation_period")
+	var sliGuid = d.Get(SLI_GUID_FIELD).(string)
+	rnd := strconv.Itoa(rand.Int())
+	d.SetId(sliGuid + rnd)
 
-    var err error
-    if tOk && eOk {
-        toleratedBudgetConsumption := d.Get("custom_tolerated_budget_consumption").(float64)
-        evaluationPeriod := d.Get("custom_evaluation_period").(int)
-        var customAlertThreshold = calculateThreshold(sloTarget, toleratedBudgetConsumption, sloPeriod, evaluationPeriod)
+	var sloPeriod = d.Get(SLO_PERIOD_FIELD).(int)
+	var sloTarget = d.Get(SLO_TARGET_FIELD).(float64)
+	var alertType = d.Get(ALERT_TYPE_FIELD).(string)
 
-        err = d.Set("custom_threshold", customAlertThreshold)
+	_, tOk := d.GetOk(CUSTOM_CONSUMPTION_FIELD)
+	_, eOk := d.GetOk(CUSTOM_EVALUTION_PERIOD_FIELD)
 
-        if err != nil {
-            return diag.FromErr(err)
-        }
-    }
+	var err error
 
-    fastBurnThreshold := calculateThreshold(sloTarget, 2, sloPeriod, 60)
-    err = d.Set("fast_burn_threshold", fastBurnThreshold)
-    if err != nil {
-        return diag.FromErr(err)
-    }
-    err = d.Set("fast_burn_evaluation_period", 60)
-    if err != nil {
-        return diag.FromErr(err)
-    }
+	switch alertType {
+	case FAST_BURN:
+		if tOk || eOk {
+			return diag.Errorf("For " + FAST_BURN + " alert type do not fill '" + CUSTOM_EVALUTION_PERIOD_FIELD + "' or '" + CUSTOM_CONSUMPTION_FIELD + "'.")
+		}
 
-    slowBurnAlertThreshold := calculateThreshold(sloTarget, 2, sloPeriod, 600)
-    err = d.Set("slow_burn_threshold", slowBurnAlertThreshold)
-    if err != nil {
-        return diag.FromErr(err)
-    }
-    err = d.Set("slow_burn_evaluation_period", 600)
-    if err != nil {
-        return diag.FromErr(err)
-    }
+		threshold := calculateThreshold(sloTarget, FAST_BURN_CONSUMPTION, sloPeriod, FAST_BURN_PERIOD)
+		err = d.Set(THRESHOLD_FIELD, threshold)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		err = d.Set(EVALUATION_PERIOD_FIELD, FAST_BURN_PERIOD)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		err = d.Set(CONSUMPTION_FIELD, FAST_BURN_CONSUMPTION)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	case CUSTOM:
+		if !tOk || !eOk {
+			return diag.Errorf("For " + CUSTOM + " alert type the fields '" + CUSTOM_EVALUTION_PERIOD_FIELD + "' and '" + CUSTOM_CONSUMPTION_FIELD + "' are mandatory.")
+		}
+
+		toleratedBudgetConsumption := d.Get(CUSTOM_CONSUMPTION_FIELD).(float64)
+		evaluationPeriod := d.Get(CUSTOM_EVALUTION_PERIOD_FIELD).(int)
+		var threshold = calculateThreshold(sloTarget, toleratedBudgetConsumption, sloPeriod, evaluationPeriod)
+
+		err = d.Set(THRESHOLD_FIELD, threshold)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		err = d.Set(EVALUATION_PERIOD_FIELD, evaluationPeriod)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		err = d.Set(CONSUMPTION_FIELD, toleratedBudgetConsumption)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+	}
+
+	err = d.Set(NRQL_FIELD, "FROM Metric SELECT 100 - clamp_max(sum(newrelic.sli.good) / sum(newrelic.sli.valid) * 100, 100) as 'SLO compliance'  WHERE sli.guid = '"+sliGuid+"'")
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
 	return nil
 }
 
 func calculateThreshold(sloTarget float64, toleratedBudgetConsumption float64, sloPeriod int, evaluationPeriod int) float64 {
 	return (100.0 - sloTarget) * ((toleratedBudgetConsumption / 100 * float64(sloPeriod) * 24) / (float64(evaluationPeriod) / 60.0))
 }
-
--- newrelic/data_source_newrelic_service_level_alert_helper_test.go
+++ newrelic/data_source_newrelic_service_level_alert_helper_test.go
@@ -1,22 +1,263 @@
-//go:build unit
-// +build unit
+//go:build integration
+// +build integration
 
 package newrelic
 
 import (
+	"fmt"
 	"testing"
+	"regexp"
 
-	"github.com/stretchr/testify/require"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestCalculateAlertThreshold(t *testing.T) {
-	var sloPeriod = 28
-	var sloTarget = 99.9
-	var toleratedBudgetConsumption = 2.0
-	var evaluationPeriod = 60
+func TestAccNewRelicServiceLevelAlertHelper_FastBurn(t *testing.T) {
+	resourceName := "data.newrelic_service_level_alert_helper.fast"
 
-	threshold := calculateAlertThreshold(sloTarget, toleratedBudgetConsumption, sloPeriod, evaluationPeriod)
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicServiceLevelAlertHelperFastBurnConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicServiceLevelAlertHelper_FastBurn(resourceName),
+				),
+			},
+		},
+	})
+}
 
-	require.NotNil(t, threshold)
-	require.Equal(t, 1.3439999999999237, threshold)
+func TestAccNewRelicServiceLevelAlertHelper_Custom(t *testing.T) {
+	resourceName := "data.newrelic_service_level_alert_helper.custom"
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicServiceLevelAlertHelperCustomConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNewRelicServiceLevelAlertHelper_Custom(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNewRelicServiceLevelAlertHelper_FastBurnError(t *testing.T) {
+    expectedErrorMessage := regexp.MustCompile(`For fast_burn alert type do not fill 'custom_evaluation_period' or 'custom_tolerated_budget_consumption'.`)
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicServiceLevelAlertHelperFastBurnEvaluationErrorConfig(),
+                ExpectError: expectedErrorMessage,
+			},
+			{
+				Config: testAccNewRelicServiceLevelAlertHelperFastBurnBudgetErrorConfig(),
+                ExpectError: expectedErrorMessage,
+			},
+		},
+	})
+}
+
+func TestAccNewRelicServiceLevelAlertHelper_CustomError(t *testing.T) {
+    expectedErrorMessage := regexp.MustCompile(`For custom alert type the fields 'custom_evaluation_period' and 'custom_tolerated_budget_consumption' are mandatory.`)
+
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNewRelicServiceLevelAlertHelperCustomEvaluationErrorConfig(),
+                ExpectError: expectedErrorMessage,
+			},
+			{
+				Config: testAccNewRelicServiceLevelAlertHelperCustomBudgetErrorConfig(),
+                ExpectError: expectedErrorMessage,
+			},
+		},
+	})
+}
+
+func testAccNewRelicServiceLevelAlertHelperFastBurnConfig() string {
+	return fmt.Sprintf(`
+data "newrelic_service_level_alert_helper" "fast" {
+    alert_type = "fast_burn"
+    sli_guid = "sliGuid"
+    slo_target = 99.9
+    slo_period = 28
+}
+`)
+}
+
+func testAccNewRelicServiceLevelAlertHelperFastBurnEvaluationErrorConfig() string {
+	return fmt.Sprintf(`
+data "newrelic_service_level_alert_helper" "fastBad" {
+    alert_type = "fast_burn"
+    sli_guid = "sliGuid"
+    slo_target = 99.9
+    slo_period = 28
+    custom_evaluation_period = 12
+}
+`)
+}
+
+func testAccNewRelicServiceLevelAlertHelperFastBurnBudgetErrorConfig() string {
+	return fmt.Sprintf(`
+data "newrelic_service_level_alert_helper" "fastBad" {
+    alert_type = "fast_burn"
+    sli_guid = "sliGuid"
+    slo_target = 99.9
+    slo_period = 28
+    custom_tolerated_budget_consumption = 34
+}
+`)
+}
+
+func testAccNewRelicServiceLevelAlertHelperCustomEvaluationErrorConfig() string {
+	return fmt.Sprintf(`
+data "newrelic_service_level_alert_helper" "customBad" {
+    alert_type = "custom"
+    sli_guid = "sliGuid"
+    slo_target = 99.9
+    slo_period = 28
+    custom_evaluation_period = 12
+}
+`)
+}
+
+func testAccNewRelicServiceLevelAlertHelperCustomBudgetErrorConfig() string {
+	return fmt.Sprintf(`
+data "newrelic_service_level_alert_helper" "customBad" {
+    alert_type = "custom"
+    sli_guid = "sliGuid"
+    slo_target = 99.9
+    slo_period = 28
+    custom_tolerated_budget_consumption = 34
+}
+`)
+}
+
+func testAccCheckNewRelicServiceLevelAlertHelper_FastBurn(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		rs, ok := s.RootModule().Resources[n]
+		// fmt.Println(s.RootModule())
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		a := rs.Primary.Attributes
+	
+        var err error
+		if err = runTest(a, "slo_period", "28"); err != nil {
+			return err
+		}
+		if err = runTest(a, "slo_target", "99.9"); err != nil {
+			return err
+		}
+		if err = runTest(a, "alert_type", "fast_burn"); err != nil {
+			return err
+		}
+		if err = runTest(a, "custom_evaluation_period", ""); err != nil {
+			return err
+		}
+		if err = runTest(a, "custom_tolerated_budget_consumption", ""); err != nil {
+			return err
+		}
+		if err = runTest(a, "evaluation_period", "60"); err != nil {
+			return err
+		}
+		if err = runTest(a, "tolerated_budget_consumption", "2"); err != nil {
+			return err
+		}
+		if err = runTest(a, "threshold", "1.3439999999999237"); err != nil {
+			return err
+		}
+		if err = runTest(a, "sli_guid", "sliGuid"); err != nil {
+			return err
+		}
+		if err = runTest(a, "nrql", "FROM Metric SELECT 100 - clamp_max(sum(newrelic.sli.good) / sum(newrelic.sli.valid) * 100, 100) as 'SLO compliance'  WHERE sli.guid = 'sliGuid'"); err != nil {
+			return err
+		}
+            
+
+		return nil
+	}
+}
+
+func testAccNewRelicServiceLevelAlertHelperCustomConfig() string {
+	return fmt.Sprintf(`
+data "newrelic_service_level_alert_helper" "custom" {
+    alert_type = "custom"
+    sli_guid = "sliGuidCustom"
+    slo_target = 98
+    slo_period = 7
+    custom_tolerated_budget_consumption = 5
+    custom_evaluation_period = 120
+}
+`)
+}
+
+func testAccCheckNewRelicServiceLevelAlertHelper_Custom(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		rs, ok := s.RootModule().Resources[n]
+		// fmt.Println(s.RootModule())
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		a := rs.Primary.Attributes
+	
+        var err error
+		if err = runTest(a, "slo_period", "7"); err != nil {
+			return err
+		}
+		if err = runTest(a, "slo_target", "98"); err != nil {
+			return err
+		}
+		if err = runTest(a, "alert_type", "custom"); err != nil {
+			return err
+		}
+		if err = runTest(a, "custom_evaluation_period", "120"); err != nil {
+			return err
+		}
+		if err = runTest(a, "custom_tolerated_budget_consumption", "5"); err != nil {
+			return err
+		}
+		if err = runTest(a, "evaluation_period", "120"); err != nil {
+			return err
+		}
+		if err = runTest(a, "tolerated_budget_consumption", "5"); err != nil {
+			return err
+		}
+		if err = runTest(a, "threshold", "8.4"); err != nil {
+			return err
+		}
+		if err = runTest(a, "sli_guid", "sliGuidCustom"); err != nil {
+			return err
+		}
+		if err = runTest(a, "nrql", "FROM Metric SELECT 100 - clamp_max(sum(newrelic.sli.good) / sum(newrelic.sli.valid) * 100, 100) as 'SLO compliance'  WHERE sli.guid = 'sliGuidCustom'"); err != nil {
+			return err
+		}
+            
+
+		return nil
+	}
+}
+
+func runTest(attributes map[string]string, name string, expected string) error {
+	actual := attributes[name]
+	if actual != expected {
+		return fmt.Errorf("Expected %s was %s, actual was %s", name, expected, actual)
+	}
+	return nil
 }
