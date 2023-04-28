39e024cca7d7911abcbfecadd21524a7eac853e9
Sander Blue <sblue@sanderblue.com>
2023-04-28 12:16:53 -0500

    chore(nrql_alert_condition): add missing check for err

--- newrelic/resource_newrelic_nrql_alert_condition.go
+++ newrelic/resource_newrelic_nrql_alert_condition.go
@@ -406,6 +406,10 @@ func resourceNewRelicNrqlAlertConditionCreate(ctx context.Context, d *schema.Res
 		return diags
 	}
 
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
 	if condition == nil {
 		return diag.Errorf("error creating nrql alert condition: response was nil")
 	}
