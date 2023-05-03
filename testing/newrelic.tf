2984f8a9e65b4bbd39e42e7b446e0384bc5a96e2
pranav-new-relic <pnandula@newrelic.com>
2023-05-03 16:31:40 +0530

    docs(dashboard): a sample to demonstrate dashboards receiving a dynamic list of pages

--- testing/newrelic.tf
+++ testing/newrelic.tf
@@ -9,3 +9,30 @@ terraform {
 provider "newrelic" {
   region = "US" # US or EU
 }
+
+resource "newrelic_one_dashboard_json" "dashboard_one" {
+  json = templatefile("dashboard.json.tftpl", {
+    name        = "2180 Multipage Dashboard One",
+    description = "A multipage dashboard that helps view multiple things at once.",
+    permissions = "PUBLIC_READ_WRITE",
+    pages       = ["page_one.json", "page_two.json"]
+  })
+}
+
+resource "newrelic_one_dashboard_json" "dashboard_two" {
+  json = templatefile("dashboard.json.tftpl", {
+    name        = "2180 Multipage Dashboard Two",
+    description = "A multipage dashboard that helps view multiple things at once.",
+    permissions = "PUBLIC_READ_WRITE",
+    pages       = ["page_two.json", "page_three.json"]
+  })
+}
+
+resource "newrelic_one_dashboard_json" "dashboard_three" {
+  json = templatefile("dashboard.json.tftpl", {
+    name        = "2180 Multipage Dashboard Three",
+    description = "A multipage dashboard that helps view multiple things at once.",
+    permissions = "PUBLIC_READ_WRITE",
+    pages       = ["page_one.json", "page_two.json", "page_three.json"]
+  })
+}
