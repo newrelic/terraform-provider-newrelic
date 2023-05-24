6fca8f7bde90daa47a085704d161a4e6020c5234
pranav-new-relic <pnandula@newrelic.com>
2023-05-24 09:47:10 +0530

    chore(imports): refactoring imports in files showing lint warnings

--- newrelic/data_source_newrelic_obfuscation_expression.go
+++ newrelic/data_source_newrelic_obfuscation_expression.go
@@ -3,12 +3,12 @@ package newrelic
 import (
 	"context"
 	"errors"
-	"github.com/newrelic/newrelic-client-go/v2/newrelic"
-	"github.com/newrelic/newrelic-client-go/v2/pkg/logconfigurations"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/newrelic/newrelic-client-go/v2/newrelic"
+	"github.com/newrelic/newrelic-client-go/v2/pkg/logconfigurations"
 )
 
 func dataSourceNewRelicObfuscationExpression() *schema.Resource {
