9ab53fb7e3c119149fc5123d607b03505bf67ab9
pranav-new-relic <127438038+pranav-new-relic@users.noreply.github.com>
2023-09-20 14:45:49 +0530

    fix(tests): change to expected error message in provider region integration test (#2473)

--- newrelic/provider_integration_test.go
+++ newrelic/provider_integration_test.go
@@ -29,6 +29,8 @@ func TestAccNewRelicProvider_Region(t *testing.T) {
 	// This error message will occur when configuring
 	// US region with EU API URLs when using the TF test account.
 	expectedErrorMsg := "403 response returned"
+	expectedErrorMsgTwo := "Access denied."
+	expectedErrorMsgRegex := fmt.Sprintf("%s|%s", expectedErrorMsg, expectedErrorMsgTwo)
 	rName := acctest.RandString(5)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -43,12 +45,12 @@ func TestAccNewRelicProvider_Region(t *testing.T) {
 			// Test: Region "EU"
 			{
 				Config:      testAccNewRelicProviderConfig("EU", "", rName),
-				ExpectError: regexp.MustCompile(expectedErrorMsg),
+				ExpectError: regexp.MustCompile(expectedErrorMsgRegex),
 			},
 			// Test: Override US region URLs with EU region URLs (will result in an auth error)
 			{
 				Config:      testAccNewRelicProviderConfig("US", `nerdgraph_api_url = "https://api.eu.newrelic.com/graphql"`, rName),
-				ExpectError: regexp.MustCompile(expectedErrorMsg),
+				ExpectError: regexp.MustCompile(expectedErrorMsgRegex),
 			},
 			// Test: Override EU region URLs with US region URLs (should work since the TF acct is US-based)
 			{
