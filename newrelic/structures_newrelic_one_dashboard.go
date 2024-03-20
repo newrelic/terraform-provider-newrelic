newrelic/terraform-provider-newrelic

commit e9d1863e70b8f96ffc8fab2336757f0474f245db

feat(dashboards): Added ignore time picker as options field to the da… (#2597)
Co-authored-by: pranav-new-relic <pnandula@newrelic.com>

newrelic/structures_newrelic_one_dashboard.go
@@ -86,6 +86,10 @@ func expandDashboardVariablesInput(variables []interface{}) []dashboards.Dashboa
 			variable.Type = dashboards.DashboardVariableType(strings.ToUpper(ty.(string)))
 		}
 
+		if options, ok := v["options"]; ok && len(options.([]interface{})) > 0 {
+			variable.Options = expandVariableOptions(options.([]interface{}))
+		}
+
 		expanded[i] = variable
 	}
 	return expanded
@@ -708,6 +712,19 @@ func expandDashboardWidgetNRQLQueryInput(queries []interface{}, meta interface{}
 	return expanded, nil
 }
 
+func expandVariableOptions(in []interface{}) *dashboards.DashboardVariableOptionsInput {
+	var out dashboards.DashboardVariableOptionsInput
+
+	for _, v := range in {
+		cfg := v.(map[string]interface{})
+		out = dashboards.DashboardVariableOptionsInput{
+			IgnoreTimeRange: cfg["ignore_time_range"].(bool),
+		}
+	}
+
+	return &out
+}
+
 // Unpack the *dashboards.Dashboard variable and set resource data.
 //
 // Used by the newrelic_one_dashboard Read function (resourceNewRelicOneDashboardRead)
@@ -794,6 +811,9 @@ func flattenDashboardVariable(in *[]entities.DashboardVariable) []interface{} {
 		m["replacement_strategy"] = strings.ToLower(string(v.ReplacementStrategy))
 		m["title"] = v.Title
 		m["type"] = strings.ToLower(string(v.Type))
+		if v.Options != nil {
+			m["options"] = flattenVariableOptions(v.Options)
+		}
 
 		out[i] = m
 	}
@@ -829,6 +849,18 @@ func flattenVariableNRQLQuery(in *entities.DashboardVariableNRQLQuery) []interfa
 
 	n["account_ids"] = in.AccountIDs
 	n["query"] = in.Query
+
+	out[0] = n
+
+	return out
+}
+
+func flattenVariableOptions(in *entities.DashboardVariableOptions) []interface{} {
+	out := make([]interface{}, 1)
+
+	n := make(map[string]interface{})
+
+	n["ignore_time_range"] = in.IgnoreTimeRange
 
 	out[0] = n
 

go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/newrelic/go-agent/v3 v3.29.1
 	github.com/newrelic/go-insights v1.0.3
-	github.com/newrelic/newrelic-client-go/v2 v2.26.0
+	github.com/newrelic/newrelic-client-go/v2 v2.26.1-0.20240313072141-eaba3e9afa15
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a
 )

go.sum
@@ -263,8 +263,8 @@ github.com/newrelic/go-agent/v3 v3.29.1 h1:OINNRev5ImiyRq0IUYwhfTmtqQgQFYyDNQEtb
 github.com/newrelic/go-agent/v3 v3.29.1/go.mod h1:9utrgxlSryNqRrTvII2XBL+0lpofXbqXApvVWPpbzUg=
 github.com/newrelic/go-insights v1.0.3 h1:zSNp1CEZnXktzSIEsbHJk8v6ZihdPFP2WsO/fzau3OQ=
 github.com/newrelic/go-insights v1.0.3/go.mod h1:A20BoT8TNkqPGX2nS/Z2fYmKl3Cqa3iKZd4whzedCY4=
-github.com/newrelic/newrelic-client-go/v2 v2.26.0 h1:5sZxMbe2UzQdOG/43ypb7NnMtKiHS4xAZJ/uMfWmVgk=
-github.com/newrelic/newrelic-client-go/v2 v2.26.0/go.mod h1:SO5KJuFJ/+l3lT8nOdNLTrcE9FoZ4m60kechCVb+1N4=
+github.com/newrelic/newrelic-client-go/v2 v2.26.1-0.20240313072141-eaba3e9afa15 h1:ap7hiGzYXo9S5Hj4mSEuWdKxf3jpE0C+AiZRkc5oslo=
+github.com/newrelic/newrelic-client-go/v2 v2.26.1-0.20240313072141-eaba3e9afa15/go.mod h1:SO5KJuFJ/+l3lT8nOdNLTrcE9FoZ4m60kechCVb+1N4=
 github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e/go.mod h1:zD1mROLANZcx1PVRCS0qkT7pwLkGfwJo4zjcN/Tysno=
 github.com/oklog/run v1.0.0 h1:Ru7dDtJNOyC66gQ5dQmaCa0qIsAUFY3sFpK1Xk8igrw=
 github.com/oklog/run v1.0.0/go.mod h1:dlhp/R75TPv97u0XWUtDeV/lRKWPKSdTuV0TZvrmrQA=

newrelic/resource_newrelic_one_dashboard.go
@@ -113,6 +113,20 @@ func dashboardVariableSchemaElem() *schema.Resource {
 				Required:    true,
 				Description: "The variable identifier.",
 			},
+			"options": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Options applied to the variable.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ignore_time_range": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Only applies to variables of type NRQL. With this turned on, the time range for the NRQL query will override the time picker on dashboards and other pages. Turn this off to use the time picker as normal.",
+						},
+					},
+				},
+			},
 			"nrql_query": {
 				Type:        schema.TypeList,
 				Optional:    true,

newrelic/resource_newrelic_one_dashboard_json_test.go
@@ -161,6 +161,9 @@ func testAccCheckNewRelicOneDashboardJsonConfig_Full(pageName string, accountID 
 					"query": "FROM Transaction SELECT average(duration) FACET appName"
 				},
 				"name": "variableNRQL",
+				"options": {
+					"ignoreTimeRange": true
+				},
 				"replacementStrategy": "DEFAULT",
 				"title": "title",
 				"type": "NRQL"

commit d54cc5ad00b1220d43c4a62b733807d55e4d488a

fix(dashboards): fixes to state diffs caused by #2597

newrelic/structures_newrelic_one_dashboard.go
@@ -747,7 +747,7 @@ func flattenDashboardEntity(dashboard *entities.DashboardEntity, d *schema.Resou
 	}
 
 	if dashboard.Variables != nil && len(dashboard.Variables) > 0 {
-		variables := flattenDashboardVariable(&dashboard.Variables)
+		variables := flattenDashboardVariable(&dashboard.Variables, d)
 		if err := d.Set("variable", variables); err != nil {
 			return err
 		}
@@ -784,7 +784,7 @@ func flattenDashboardUpdateResult(result *dashboards.DashboardUpdateResult, d *s
 	}
 
 	if dashboard.Variables != nil && len(dashboard.Variables) > 0 {
-		variables := flattenDashboardVariable(&dashboard.Variables)
+		variables := flattenDashboardVariable(&dashboard.Variables, d)
 		if err := d.Set("variable", variables); err != nil {
 			return err
 		}
@@ -793,7 +793,7 @@ func flattenDashboardUpdateResult(result *dashboards.DashboardUpdateResult, d *s
 	return nil
 }
 
-func flattenDashboardVariable(in *[]entities.DashboardVariable) []interface{} {
+func flattenDashboardVariable(in *[]entities.DashboardVariable, d *schema.ResourceData) []interface{} {
 	out := make([]interface{}, len(*in))
 
 	for i, v := range *in {
@@ -812,7 +812,12 @@ func flattenDashboardVariable(in *[]entities.DashboardVariable) []interface{} {
 		m["title"] = v.Title
 		m["type"] = strings.ToLower(string(v.Type))
 		if v.Options != nil {
-			m["options"] = flattenVariableOptions(v.Options)
+			options := flattenVariableOptions(v.Options, d, i)
+			if options != nil {
+				// set options -> ignore_time_range to the state only if they already exist in the configuration
+				// needed to make this backward compatible with configurations which do not yet have options -> ignore_time_range
+				m["options"] = options
+			}
 		}
 
 		out[i] = m
@@ -855,15 +860,26 @@ func flattenVariableNRQLQuery(in *entities.DashboardVariableNRQLQuery) []interfa
 	return out
 }
 
-func flattenVariableOptions(in *entities.DashboardVariableOptions) []interface{} {
+func flattenVariableOptions(in *entities.DashboardVariableOptions, d *schema.ResourceData, index int) []interface{} {
+	// fetching the contents of the variable at the specified index (in the list of variables)
+	// and subsequently, finding its options field
+	variableFetched := d.Get(fmt.Sprintf("variable.%d", index)).(map[string]interface{})
+	optionsOfVariableFetched := variableFetched["options"]
+	options := optionsOfVariableFetched.([]interface{})
+
+	if len(options) == 0 {
+		// if nothing exists in the options list in the state (configuration), "do nothing", to avoid drift
+		// this is required to make options -> ignore_time_range backward compatible and show no drift
+		// when customer configuration does not comprise these attributes
+		return nil
+	}
+
+	// else, only if the options list has something, get the value of ignore_time_range
+	// (set it to the value of ignore_time_range seen in the response returned by the API)
 	out := make([]interface{}, 1)
-
 	n := make(map[string]interface{})
-
 	n["ignore_time_range"] = in.IgnoreTimeRange
-
 	out[0] = n
-
 	return out
 }
 

website/docs/r/one_dashboard.html.markdown
@@ -331,6 +331,8 @@ The following arguments are supported:
   * `replacement_strategy` - (Optional) Indicates the strategy to apply when replacing a variable in a NRQL query. One of `default`, `identifier`, `number` or `string`.
   * `title` - (Optional) Human-friendly display string for this variable.
   * `type` - (Required) Specifies the data type of the variable and where its possible values may come from. One of `enum`, `nrql` or `string`
+  * `options` - (Optional) Specifies additional options to be added to dashboard variables. Supports the following nested attribute(s) -
+    * `ignore_time_range` - (Optional) An argument with a boolean value that is supported only by variables of `type` _nrql_ - when true, the time range specified in the query will override the time picker on dashboards and other pages.
 
 ### Nested `item` blocks
 