5231e4df82844b7d3d4a1b17fd5b2cee16775cce
pranav-new-relic <pnandula@newrelic.com>
2024-05-07 16:57:17 +0530

    chore: undo an accidental change

--- testing/newrelic.tf
+++ testing/newrelic.tf
@@ -9,9 +9,3 @@ terraform {
 provider "newrelic" {
   region = "US" # US or EU
 }
-
-resource "newrelic_cloud_aws_link_account" "foo" {
-  arn = "arn:aws:iam::709144918866:role/NewRelicInfrastructure-Integrations-V2"
-  metric_collection_mode = "PUSH"
-  name = "foo"
-}
