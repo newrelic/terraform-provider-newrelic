87f4bf542d1a2d3f2ba6daf6be9eab64d2acbf38
Vinay Reddy Godhala <vgodhala@newrelic.com>
2024-08-22 17:46:51 +0530

    fix(dashboard): Adding a proper error message to the default values field while creating dashboard

--- newrelic/structures_newrelic_one_dashboard.go
+++ newrelic/structures_newrelic_one_dashboard.go
@@ -35,7 +35,10 @@ func expandDashboardInput(d *schema.ResourceData, meta interface{}, dashboardNam
 		return nil, err
 	}
 
-	dash.Variables = expandDashboardVariablesInput(d.Get("variable").([]interface{}))
+	dash.Variables, err = expandDashboardVariablesInput(d.Get("variable").([]interface{}))
+	if err != nil {
+		return nil, err
+	}
 
 	// Optional, with default
 	perm := d.Get("permissions").(string)
@@ -49,9 +52,9 @@ func expandDashboardInput(d *schema.ResourceData, meta interface{}, dashboardNam
 	return &dash, nil
 }
 
-func expandDashboardVariablesInput(variables []interface{}) []dashboards.DashboardVariableInput {
+func expandDashboardVariablesInput(variables []interface{}) ([]dashboards.DashboardVariableInput, error) {
 	if len(variables) < 1 {
-		return []dashboards.DashboardVariableInput{}
+		return []dashboards.DashboardVariableInput{}, nil
 	}
 
 	expanded := make([]dashboards.DashboardVariableInput, len(variables))
@@ -60,8 +63,22 @@ func expandDashboardVariablesInput(variables []interface{}) []dashboards.Dashboa
 		var variable dashboards.DashboardVariableInput
 		v := val.(map[string]interface{})
 
-		if d, ok := v["default_values"]; ok && len(d.([]interface{})) > 0 {
-			variable.DefaultValues = expandVariableDefaultValues(d.([]interface{}))
+		if d, ok := v["default_values"].([]interface{}); ok && d != nil {
+			// Check if the slice is empty or contains only nil elements as we are receiving d as [<nil>] when an empty string is given
+			hasNonNil := false
+			for _, item := range d {
+				if item != nil {
+					hasNonNil = true
+					break
+				}
+			}
+			if len(d) > 0 && hasNonNil {
+				variable.DefaultValues = expandVariableDefaultValues(d)
+			} else {
+				return []dashboards.DashboardVariableInput{}, fmt.Errorf("default_values is an empty slice or contains only nil values")
+			}
+		} else {
+			return []dashboards.DashboardVariableInput{}, fmt.Errorf("default_values is either nil, not a slice, or an empty slice")
 		}
 
 		if m, ok := v["is_multi_selection"]; ok {
@@ -98,7 +115,7 @@ func expandDashboardVariablesInput(variables []interface{}) []dashboards.Dashboa
 
 		expanded[i] = variable
 	}
-	return expanded
+	return expanded, nil
 }
 
 func expandVariableDefaultValues(in []interface{}) *[]dashboards.DashboardVariableDefaultItemInput {
