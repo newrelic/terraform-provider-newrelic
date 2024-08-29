4633984c3f5c853e4e7ec4156375385ceec681cc
Vinay Reddy Godhala <vgodhala@newrelic.com>
2024-08-30 01:45:06 +0530

    fix(dashboard): fixed PR Review comments

--- newrelic/structures_newrelic_one_dashboard.go
+++ newrelic/structures_newrelic_one_dashboard.go
@@ -63,7 +63,7 @@ func expandDashboardVariablesInput(variables []interface{}) ([]dashboards.Dashbo
 		var variable dashboards.DashboardVariableInput
 		v := val.(map[string]interface{})
 
-		if d, ok := v["default_values"].([]interface{}); ok && d != nil && len(d) > 0 {
+		if d, ok := v["default_values"].([]interface{}); ok && d != nil && len(d) >= 0 {
 			// Check if the slice is empty or contains only nil elements as we are receiving d as [<nil>] when an empty string is given
 			hasNil := false
 			for _, item := range d {
