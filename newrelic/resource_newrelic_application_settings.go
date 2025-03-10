0802fd7898fff36be961252fb6ab4a08e6ac088f
vinay-newrelic <vgodhala@newrelic.com>
2025-03-10 15:45:44 +0530

    feat(application_settings) : Revamp resource to support all latest APM Application Settings (#2799)
    Co-authored-by: pranav-new-relic <pnandula@newrelic.com>

--- newrelic/resource_newrelic_application_settings.go
+++ newrelic/resource_newrelic_application_settings.go
@@ -2,13 +2,15 @@ package newrelic
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"strconv"
-	"time"
+
+	"github.com/newrelic/newrelic-client-go/v2/pkg/agentapplications"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/newrelic/newrelic-client-go/v2/pkg/apm"
+	"github.com/newrelic/newrelic-client-go/v2/pkg/common"
+	"github.com/newrelic/newrelic-client-go/v2/pkg/entities"
 )
 
 func resourceNewRelicApplicationSettings() *schema.Resource {
@@ -20,99 +22,137 @@ func resourceNewRelicApplicationSettings() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"app_apdex_threshold": {
-				Type:     schema.TypeFloat,
-				Required: true,
-			},
-			"end_user_apdex_threshold": {
-				Type:     schema.TypeFloat,
-				Required: true,
-			},
-			"enable_real_user_monitoring": {
-				Type:     schema.TypeBool,
-				Required: true,
-			},
-		},
+		Schema: mergeSchemas(
+			applicationSettingCommonSchema(),
+			apmApplicationSettingsSchema(),
+		),
+		CustomizeDiff: validateApplicationSettingsInput,
 	}
 }
 
 func resourceNewRelicApplicationSettingsCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*ProviderConfig).NewClient
 
-	userApp := expandApplication(d)
-
-	listParams := apm.ListApplicationsParams{
-		Name: userApp.Name,
-	}
-
-	result, err := client.APM.ListApplicationsWithContext(ctx, &listParams)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	var app *apm.Application
-
-	for _, a := range result {
-		if a.Name == userApp.Name {
-			app = a
-			break
-		}
-	}
-
-	if app == nil {
-		return diag.Errorf("the name '%s' does not match any New Relic applications", userApp.Name)
-	}
-
-	d.SetId(strconv.Itoa(app.ID))
-
-	log.Printf("[INFO] Importing New Relic application %v", userApp.Name)
 	return resourceNewRelicApplicationSettingsUpdate(ctx, d, meta)
 }
 
 func resourceNewRelicApplicationSettingsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*ProviderConfig).NewClient
 
-	userApp := expandApplication(d)
-	log.Printf("[INFO] Reading New Relic application %+v", userApp)
+	providerConfig := meta.(*ProviderConfig)
+	client := providerConfig.NewClient
 
-	app, err := client.APM.GetApplicationWithContext(ctx, userApp.ID)
+	log.Printf("[INFO] Reading New Relic application %+v", d.Id())
+
+	resp, err := client.Entities.GetEntityWithContext(ctx, common.EntityGUID(d.Id()))
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	log.Printf("[INFO] Read found New Relic application %+v\n\n\n", app)
+	if resp == nil {
+		d.SetId("")
+		return diag.FromErr(fmt.Errorf("no New Relic application found with given guid %s", d.Id()))
+	}
 
-	return diag.FromErr(flattenApplication(app, d))
+	var dig diag.Diagnostics
+	switch (*resp).(type) {
+	case *entities.ApmApplicationEntity:
+		entity := (*resp).(*entities.ApmApplicationEntity)
+		d.SetId(string(entity.GUID))
+		_ = d.Set("guid", string(entity.GUID))
+		dig = diag.FromErr(setAPMApplicationValues(d, entity.ApmSettings))
+	default:
+		dig = diag.FromErr(fmt.Errorf("problem in retrieving application with GUID %s", d.Id()))
+	}
+	return dig
 }
 
 func resourceNewRelicApplicationSettingsUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*ProviderConfig).NewClient
 
-	userApp := expandApplication(d)
-
-	updateParams := apm.UpdateApplicationParams{
-		Name:     userApp.Name,
-		Settings: userApp.Settings,
+	_, guidExists := d.GetOk("guid")
+	_, nameExists := d.GetOk("name")
+	if nameExists && !guidExists {
+		entityRes, err := getEntityDetailsFromName(ctx, d, meta)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		if entityRes == nil {
+			return diag.FromErr(fmt.Errorf("no entities found with the provided name, please ensure the name of a valid APM entity is provided"))
+		}
+		_ = d.Set("guid", string((*entityRes).GetGUID()))
 	}
 
-	log.Printf("[INFO] Updating New Relic application %+v with params: %+v", userApp, updateParams)
+	updateApplicationParams := expandApplication(d)
 
-	app, err := client.APM.UpdateApplicationWithContext(ctx, userApp.ID, updateParams)
+	guid := d.Get("guid").(string)
+	log.Printf("[INFO] Updating New Relic application %+v with params: %+v", guid, updateApplicationParams)
+
+	agentApplicationSettingResult, err := client.AgentApplications.AgentApplicationSettingsUpdate(common.EntityGUID(guid), *updateApplicationParams)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if agentApplicationSettingResult == nil {
+		return diag.FromErr(fmt.Errorf("something went wrong while Updating New Relic application"))
+	}
+
+	d.SetId(string(agentApplicationSettingResult.GUID))
+	err = d.Set("is_imported", true)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	time.Sleep(2 * time.Second)
-
-	return diag.FromErr(flattenApplication(app, d))
+	return resourceNewRelicApplicationSettingsRead(ctx, d, meta)
 }
 
 func resourceNewRelicApplicationSettingsDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// You can not delete application settings
-	return nil
+	var diags diag.Diagnostics
+	client := meta.(*ProviderConfig).NewClient
+	guid := d.Get("guid").(string)
+	falseValue := false
+
+	agentApplicationSettingResult, err := client.AgentApplications.AgentApplicationSettingsUpdate(
+		common.EntityGUID(guid),
+		agentapplications.AgentApplicationSettingsUpdateInput{
+			ApmConfig: &agentapplications.AgentApplicationSettingsApmConfigInput{
+				UseServerSideConfig: &falseValue,
+				ApdexTarget:         0.5,
+			},
+			ErrorCollector: &agentapplications.AgentApplicationSettingsErrorCollectorInput{
+				Enabled: &falseValue,
+			},
+			TransactionTracer: &agentapplications.AgentApplicationSettingsTransactionTracerInput{
+				Enabled:        &falseValue,
+				ExplainEnabled: &falseValue,
+				RecordSql:      agentapplications.AgentApplicationSettingsRecordSqlEnumTypes.OFF,
+			},
+			TracerType: &agentapplications.AgentApplicationSettingsTracerTypeInput{
+				// choosing "NONE" instead of "OPT_OUT", as OPT_OUT is not available as a type in the Go Client
+				// since OPT_OUT is not a recognized enum value by NerdGraph
+				Value: agentapplications.AgentApplicationSettingsTracerTypes.NONE,
+			},
+			ThreadProfiler: &agentapplications.AgentApplicationSettingsThreadProfilerInput{
+				Enabled: &falseValue,
+			},
+			SlowSql: &agentapplications.AgentApplicationSettingsSlowSqlInput{
+				Enabled: &falseValue,
+			},
+		},
+	)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if agentApplicationSettingResult == nil {
+		return diag.FromErr(fmt.Errorf("something went wrong while clearing the settings of the application"))
+	}
+
+	// this function should not be removed, this exists in every delete function to "clear" Terraform state
+	d.SetId("")
+
+	diags = append(diags, diag.Diagnostic{
+		Severity: diag.Warning,
+		Summary:  "\nSince the `newrelic_application_settings` resource does not support deletion via NerdGraph, the resource has been reset to its initial state and cleared of most settings.\nYou would still find some settings of this APM entity in the New Relic UI (such as the Apdex Threshold) which cannot be cleared.",
+	})
+	return diags
 }
