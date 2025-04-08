16c027cf46cd0b1eada74278b7583adda63654fd
Pranav Nandula <127438038+pranav-new-relic@users.noreply.github.com>
2025-04-08 23:16:00 +0530

    fix(build): revamp integration test framework via selective product capability tag based run processes (#2821)

--- newrelic/resource_newrelic_service_level_test.go
+++ newrelic/resource_newrelic_service_level_test.go
@@ -1,5 +1,5 @@
-//go:build integration
-// +build integration
+//go:build integration || WORKLOADS
+// +build integration WORKLOADS
 
 package newrelic
 
--- newrelic/resource_newrelic_synthetics_cert_check_monitor_test.go
+++ newrelic/resource_newrelic_synthetics_cert_check_monitor_test.go
@@ -1,4 +1,5 @@
-//go:build integration
+//go:build integration || SYNTHETICS
+// +build integration SYNTHETICS
 
 package newrelic
 
