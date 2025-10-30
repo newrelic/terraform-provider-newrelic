9caedc6164f5f1090bdd455eff3a212b2479d175
Aashirwadjain <aashirwad2001jain@gmail.com>
2025-10-30 15:02:11 +0530

    Revert "test(dashboards): add tests for showApplyAction"
    This reverts commit 383a8f2a15b0748cc06b141748cea2b140080fb0.

--- testing/newrelic.tf
+++ testing/newrelic.tf
@@ -10,34 +10,3 @@ provider "newrelic" {
   region = "US" # US or EU
 }
 
-resource "newrelic_one_dashboard" "exampledash" {
-  name        = "New Relic Terraform Example"
-  permissions = "public_read_only"
-
-  page {
-    name = "New Relic Terraform Example"
-  }
-
-  variable {
-    is_multi_selection = true
-    item {
-      title = "item"
-      value = "ITEM"
-    }
-    name = "variable"
-    nrql_query {
-      account_ids = [3806526]
-      query       = "FROM Transactions SELECT uniques(duration)"
-    }
-    replacement_strategy = "default"
-    title                = "title"
-    type                 = "nrql"
-
-    # options {
-    #   excluded = true
-    #   ignore_time_range = true
-    #   # show_apply_action = true
-    # }
-
-  }
-}
