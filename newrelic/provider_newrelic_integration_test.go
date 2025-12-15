6793733061b98739228682e9161712bae0199400
Gabe Fuentes <gfuentes@newrelic.com>
2025-12-15 15:39:57 -0800

    chore(alerts): remove change for local testing

--- newrelic/provider_newrelic_integration_test.go
+++ newrelic/provider_newrelic_integration_test.go
@@ -15,11 +15,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-// func TestProvider(t *testing.T) {
-// 	if err := Provider().InternalValidate(); err != nil {
-// 		t.Fatalf("err: %s", err)
-// 	}
-// }
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
 
 func TestProviderImpl(t *testing.T) {
 	var _ *schema.Provider = Provider()
