8b9be8c463bd53a0cbf00a0dce307a26ee5dd4f3
nr-developer-toolkit <nr-developer-toolkit@users.noreply.github.com>
2026-08-13 10:46:03 +0000

    feat(maintenancewindow): new terraform resource

--- newrelic/provider_newrelic.go
+++ newrelic/provider_newrelic.go
@@ -138,6 +138,7 @@ func Provider() *schema.Provider {
 			"newrelic_user":                         dataSourceNewRelicUser(),
 			"newrelic_fleet_configuration":          dataSourceNewRelicFleetConfiguration(),
 			"newrelic_fleet_members":                dataSourceNewRelicFleetMembers(),
+			"newrelic_maintenance_window": resourceNewRelicMaintenanceWindow(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
