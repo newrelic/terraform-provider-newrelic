32b31a754590b544a3d3c8ff62a5cc263a0ca9bd
jashraf-hash <jashraf@newrelic.com>
2026-09-09 19:40:26 +0530

    Changes for resource name resolution

--- examples/modules/cloud-integrations/oci/policy-setup/main.tf
+++ examples/modules/cloud-integrations/oci/policy-setup/main.tf
@@ -121,6 +121,11 @@ resource "oci_identity_policy" "nr_common_policy" {
     "Allow dynamic-group ${local.dynamic_group_name} to use fn-function in tenancy",
     "Allow dynamic-group ${local.dynamic_group_name} to use fn-invocation in tenancy",
     "Allow dynamic-group ${local.dynamic_group_name} to read secret-bundles in tenancy where any { target.secret.id = '${local.ingest_key_secret_ocid}', target.secret.id = '${local.user_key_secret_ocid}' }",
+    "Allow dynamic-group ${local.dynamic_group_name} to inspect dns-family in tenancy",
+    "Allow dynamic-group ${local.dynamic_group_name} to inspect goldengate-family in tenancy",
+    "Allow dynamic-group ${local.dynamic_group_name} to inspect network-firewall-family in tenancy",
+    "Allow dynamic-group ${local.dynamic_group_name} to inspect cloudevents-rules in tenancy",
+    "Allow dynamic-group ${local.dynamic_group_name} to inspect integration-instance in tenancy",
   ]
   defined_tags  = {}
   freeform_tags = local.freeform_tags
